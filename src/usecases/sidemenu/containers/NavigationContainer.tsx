ae83feb9bb90f323a0bb68f1d4201d7464ed451d
Peter Janson <peter.janson@elvaco.se>
2017-07-20 11:25:39 +0200

    Adding side bar navigation container
    Still needs lots of TLC when it comes to layout etc but it's a start.
    
    refs #4232

---
+++ src/usecases/sidemenu/containers/NavigationContainer.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import {Icon} from '../../common/components/icons/Icons';
+import {Column} from '../../layouts/components/column/Column';
+import {Row} from '../../layouts/components/row/Row';
+import {NavigationItem} from '../components/NavigationItem';
+
+export const NavigationContainer = props => (
+  <Row>
+    <Column>
+      <NavigationItem name="Tillbaka" icon="back"/>
+    </Column>
+    <Column>
+      <NavigationItem name="Framåt" icon="forward"/>
+    </Column>
+    <Column>
+      <Icon/>
+    </Column>
+  </Row>
+);
