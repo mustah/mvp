b3ca94f95e2887d5c4b54b71618b7cce5f1b4923
Peter Janson <peter.janson@elvaco.se>
2017-07-20 11:08:32 +0200

    Renamed SideMenuItem to LinkItem
    refs #4232

--- src/usecases/sidemenu/components/LinkItem.tsx
+++ src/usecases/sidemenu/components/LinkItem.tsx
@@ -3,21 +3,21 @@ import {Expandable} from '../../../types/Types';
 import {Icon} from '../../common/components/icons/Icons';
 import {Column} from '../../layouts/components/column/Column';
 import {Row} from '../../layouts/components/row/Row';
-import './SideMenuItem.scss';
+import './LinkItem.scss';
 
-export interface SideMenuItemProps extends Expandable {
+export interface LinkItemProps extends Expandable {
   icon: string;
   name: string;
 }
 
-export const SideMenuItem = (props: SideMenuItemProps) => {
+export const LinkItem = (props: LinkItemProps) => {
   const {name} = props;
   /* TODO : add icon for showing if node is expanded or not*/
   return (
     <Column>
       <Row>
         <Icon/>
-        <div className="SideMenuItem">{name}</div>
+        <div className="LinkItem">{name}</div>
       </Row>
     </Column>
   );
--- src/usecases/sidemenu/containers/SideMenuContainer.tsx
+++ src/usecases/sidemenu/containers/SideMenuContainer.tsx
@@ -1,24 +1,24 @@
 import * as React from 'react';
 import {Column} from '../../layouts/components/column/Column';
 import {Row} from '../../layouts/components/row/Row';
-import {SideMenuItem} from '../components/SideMenuItem';
+import {LinkItem} from '../components/LinkItem';
 
 export const SideMenuContainer = props => (
   <Column className="flex-1">
     <Row>
-      <SideMenuItem name="Sparade objekt" icon="star-rate"/>
+      <LinkItem name="Sparade objekt" icon="star-rate"/>
     </Row>
     <Row>
-      <SideMenuItem name="Sparade filter" icon="saved-filter"/>
+      <LinkItem name="Sparade filter" icon="saved-filter"/>
     </Row>
     <Row>
-      <SideMenuItem name="Dynamiska grupper" icon="dynamic-groups"/>
+      <LinkItem name="Dynamiska grupper" icon="dynamic-groups"/>
     </Row>
     <Row>
-      <SideMenuItem name="Statiska grupper" icon="static-groups"/>
+      <LinkItem name="Statiska grupper" icon="static-groups"/>
     </Row>
     <Row>
-      <SideMenuItem name="Fullständig katalog" icon="full-catalog"/>
+      <LinkItem name="Fullständig katalog" icon="full-catalog"/>
     </Row>
   </Column>
 );
