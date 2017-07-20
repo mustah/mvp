dd779edfdbdcd565f60f500816598c0c1cdb0910
Peter Janson <peter.janson@elvaco.se>
2017-07-20 11:50:24 +0200

    Adding search box to side bar
    Also needs lots of more TLC.
    
    refs #4232

---
+++ src/usecases/sidemenu/components/SearchBox.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import './LinkItem.scss';
+
+export const SearchBox = (props) => {
+  return (
+    <text>Sök i sidebar</text>
+  );
+};
---
+++ src/usecases/sidemenu/containers/SearchContainer.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import {SearchBox} from '../components/SearchBox';
+
+export const SearchContainer = props => (
+  <SearchBox/>
+);
