05c0dea572986f8e10b85110b2ed870ec5545804
Anton Lofgren <antlof@elvaco.se>
2017-08-22 15:05:59 +0200

    Serve static content from frontend package
    We want our final artifact to be as self-contained as possible, to
    simplify deployment - including the frontend as part of the artifact is
    key to achieve that.
    
    As a consequence, we no longer need the dummy security adapter, so I've
    removed that along with some obsolete templates.

--- backend/src/main/java/com/elvaco/mvp/MvpApplication.java
+++ backend/src/main/java/com/elvaco/mvp/MvpApplication.java
@@ -5,13 +5,23 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.util.StringUtils;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import org.springframework.web.servlet.resource.ResourceResolver;
+import org.springframework.web.servlet.resource.ResourceResolverChain;
 
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Locale;
 
 @SpringBootApplication
@@ -19,6 +29,69 @@ public class MvpApplication extends WebMvcConfigurerAdapter {
 
     public static void main(String[] args) {
         SpringApplication.run(MvpApplication.class, args);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/")
+                .resourceChain(false)
+                .addResolver(new PushStateResourceResolver());
+    }
+
+    private class PushStateResourceResolver implements ResourceResolver {
+        private Resource index = new ClassPathResource("/static/index.html");
+        private List<String> handledExtensions = Arrays.asList("html", "js", "json", "csv", "css", "png", "svg", "eot", "ttf", "woff", "appcache", "jpg", "jpeg", "gif", "ico");
+        private List<String> ignoredPaths = Arrays.asList("api");
+
+        @Override
+        public Resource resolveResource(HttpServletRequest request, String requestPath, List<? extends Resource> locations, ResourceResolverChain chain) {
+            return resolve(requestPath, locations);
+        }
+
+        @Override
+        public String resolveUrlPath(String resourcePath, List<? extends Resource> locations, ResourceResolverChain chain) {
+            Resource resolvedResource = resolve(resourcePath, locations);
+            if (resolvedResource == null) {
+                return null;
+            }
+            try {
+                return resolvedResource.getURL().toString();
+            } catch (IOException e) {
+                return resolvedResource.getFilename();
+            }
+        }
+
+        private Resource resolve(String requestPath, List<? extends Resource> locations) {
+            if (isIgnored(requestPath)) {
+                return null;
+            }
+            if (isHandled(requestPath)) {
+                return locations.stream()
+                        .map(loc -> createRelative(loc, requestPath))
+                        .filter(resource -> resource != null && resource.exists())
+                        .findFirst()
+                        .orElseGet(null);
+            }
+            return index;
+        }
+
+        private Resource createRelative(Resource resource, String relativePath) {
+            try {
+                return resource.createRelative(relativePath);
+            } catch (IOException e) {
+                return null;
+            }
+        }
+
+        private boolean isIgnored(String path) {
+            return ignoredPaths.contains(path);
+        }
+
+        private boolean isHandled(String path) {
+            String extension = StringUtils.getFilenameExtension(path);
+            return handledExtensions.stream().anyMatch(ext -> ext.equals(extension));
+        }
     }
 
     @Bean
@@ -47,11 +120,5 @@ public class MvpApplication extends WebMvcConfigurerAdapter {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(localeChangeInterceptor());
-    }
-
-    @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/login").setViewName("login");
-        registry.addViewController("/").setViewName("users");
     }
 }
