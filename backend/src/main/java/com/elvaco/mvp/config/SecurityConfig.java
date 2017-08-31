01ce7d0f5c498b5311c677e712e07b630e91ed27
Must Tahir <must.tahir@gmail.com>
2017-08-31 15:33:30 +0200

    Enable h2-console along side spring web security

--- backend/src/main/java/com/elvaco/mvp/config/SecurityConfig.java
+++ backend/src/main/java/com/elvaco/mvp/config/SecurityConfig.java
@@ -28,6 +28,10 @@ class SecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
+    http.authorizeRequests().antMatchers("/h2-console/**").permitAll();
+    http.csrf().disable();
+    http.headers().frameOptions().disable();
+
     http
       .authorizeRequests().antMatchers("/api/**").fullyAuthenticated()
       .and()
