669413417297531178d9c684282e7d1304e931f7
Carl Helmertz <carl.helmertz@elvaco.se>
2017-09-28 09:25:19 +0200

    config: Change back typo
    Not sure what to say. This was silly.
    
    Signed-off-by: Carl Helmertz <carl.helmertz@elvaco.se>

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,7 +1,7 @@
 import {LazyAppConfig} from './types/AppConfig';
 
 const config: LazyAppConfig = process.env.NODE_ENV === 'development'
-  ? require('./config.dev')
+  ? require('./config.dev').config
   : require('./config.prod').config;
 
 export {config};
