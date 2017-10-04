ab2f12792018356946a54d27172aa252d0da00b4
Must Tahir <must.tahir@gmail.com>
2017-10-04 20:39:42 +0200

    Move out mock data to its own file
    Import the data instead of inline it in the React components. This way
    all our merge conflicts can be more easily resolved and detected.

--- frontend/src/usecases/data-analysis/containers/DataAnalysisContainer.tsx
+++ frontend/src/usecases/data-analysis/containers/DataAnalysisContainer.tsx
@@ -6,14 +6,13 @@ import {RootState} from '../../../reducers/index';
 import {translate} from '../../../services/translationService';
 import {Image} from '../../common/components/images/Image';
 import {IndicatorWidgets, SelectedIndicatorWidgetProps} from '../../common/components/indicators/IndicatorWidgets';
-import {Indicator, IndicatorType} from '../../common/components/indicators/models/IndicatorModels';
 import {SelectionOverview} from '../../common/components/selectionoverview/SelectionOverview';
 import {Column} from '../../layouts/components/column/Column';
 import {Content} from '../../layouts/components/content/Content';
 import {Layout} from '../../layouts/components/layout/Layout';
 import {selectReportIndicatorWidget} from '../../ui/uiActions';
 import {fetchDataAnalysis} from '../dataAnalysisActions';
-import {DataAnalysisState} from '../models/DataAnalysis';
+import {DataAnalysisState, indicators} from '../models/DataAnalysis';
 import {DataAnalysisOverviewContainer} from './DataAnalysisOverviewContainer';
 
 export interface DataAnalysisContainerProps extends SelectedIndicatorWidgetProps {
@@ -21,63 +20,12 @@ export interface DataAnalysisContainerProps extends SelectedIndicatorWidgetProps
   dataAnalysis: DataAnalysisState;
 }
 
-const indicators: Indicator[] = [
-  {
-    type: IndicatorType.current,
-    title: 'El',
-    state: 'ok',
-    value: '123',
-    unit: 'kWh/m2',
-    subtitle: '(+5)',
-  },
-  {
-    type: IndicatorType.coldWater,
-    title: 'Kallvatten',
-    state: 'warning',
-    value: '53',
-    unit: 'l/m2',
-    subtitle: '(+6)',
-  },
-  {
-    type: IndicatorType.warmWater,
-    title: 'Varmvatten',
-    state: 'warning',
-    value: '13',
-    unit: 'l/m2',
-    subtitle: '(-2)',
-  },
-  {
-    type: IndicatorType.districtHeating,
-    title: 'Fjärrvärme',
-    state: 'ok',
-    value: '1.1',
-    unit: 'kWh/m2',
-    subtitle: '(-2)',
-  },
-  {
-    type: IndicatorType.temperatureInside,
-    title: 'Temp Inomhus',
-    state: 'ok',
-    value: '22.4',
-    unit: '°C',
-    subtitle: '(+0.2)',
-  },
-  {
-    type: IndicatorType.temperatureOutside,
-    title: 'Temp Utomhus',
-    state: 'info',
-    value: '13',
-    unit: '°C',
-    subtitle: '(+2)',
-  },
-];
-
 const DataAnalysisContainer = (props: DataAnalysisContainerProps & InjectedAuthRouterProps) => {
   const {fetchDataAnalysis, selectedWidget, selectIndicatorWidget} = props;
   return (
     <Layout>
       <Column className="flex-1">
-        <SelectionOverview title={'Allt'}/>
+        <SelectionOverview title={translate('all')}/>
         <Content>
           <DataAnalysisOverviewContainer/>
           <MainTitle title={translate('consumption')}/>
--- frontend/src/usecases/data-analysis/models/DataAnalysis.ts
+++ frontend/src/usecases/data-analysis/models/DataAnalysis.ts
@@ -1,6 +1,60 @@
+import {Indicator, IndicatorType} from '../../common/components/indicators/models/IndicatorModels';
+
 export interface DataAnalysisState {
   title: string;
   records: DataAnalysisState[];
   error?: string;
   isFetching: boolean;
 }
+
+// TODO[!must!] create this in redux later!
+export const indicators: Indicator[] = [
+  {
+    type: IndicatorType.current,
+    title: 'El',
+    state: 'ok',
+    value: '123',
+    unit: 'kWh/m2',
+    subtitle: '(+5)',
+  },
+  {
+    type: IndicatorType.coldWater,
+    title: 'Kallvatten',
+    state: 'warning',
+    value: '53',
+    unit: 'l/m2',
+    subtitle: '(+6)',
+  },
+  {
+    type: IndicatorType.warmWater,
+    title: 'Varmvatten',
+    state: 'warning',
+    value: '13',
+    unit: 'l/m2',
+    subtitle: '(-2)',
+  },
+  {
+    type: IndicatorType.districtHeating,
+    title: 'Fjärrvärme',
+    state: 'ok',
+    value: '1.1',
+    unit: 'kWh/m2',
+    subtitle: '(-2)',
+  },
+  {
+    type: IndicatorType.temperatureInside,
+    title: 'Temp Inomhus',
+    state: 'ok',
+    value: '22.4',
+    unit: '°C',
+    subtitle: '(+0.2)',
+  },
+  {
+    type: IndicatorType.temperatureOutside,
+    title: 'Temp Utomhus',
+    state: 'info',
+    value: '13',
+    unit: '°C',
+    subtitle: '(+2)',
+  },
+];
