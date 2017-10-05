ac7200b0624523b873fc18f23cc2fb5f96f33c58
Carl Helmertz <carl.helmertz@elvaco.se>
2017-10-05 11:50:31 +0200

    css: Remove duplicated MeteringPoint.scss
    It's a sloppy leftover.
    
    Signed-off-by: Carl Helmertz <carl.helmertz@elvaco.se>

--- frontend/src/usecases/table/components/meteringPoint/MeteringPoint.tsx
+++ frontend/src/usecases/table/components/meteringPoint/MeteringPoint.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import {Link} from 'react-router-dom';
 import {routes} from '../../../app/routes';
-import './MeteringPoint.scss';
 
 interface MeteringPointProps {
   id: string;
