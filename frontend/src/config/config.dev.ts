740b4e8ed851fe6980d9b809243c87cec7034fe9
Carl Helmertz <carl.helmertz@elvaco.se>
2017-10-10 18:03:58 +0200

    auth: Mock login (login with anything)
    You add users in frontend/src/usecases/auth/authActions.ts
    
    For now, login with "adam" as email, or anything else.
    
    Signed-off-by: Carl Helmertz <carl.helmertz@elvaco.se>

--- frontend/src/config/config.dev.ts
+++ frontend/src/config/config.dev.ts
@@ -2,6 +2,7 @@ import {AppConfig} from './AppConfig';
 
 export const config: AppConfig = {
   environment: 'development',
+  useJsonServerInsteadOfJavaBackend: true,
   axios: {
     baseURL: '//localhost:8080/api',
     timeout: 30000,
--- frontend/src/config/config.prod.ts
+++ frontend/src/config/config.prod.ts
@@ -2,6 +2,7 @@ import {AppConfig} from './AppConfig';
 
 export const config: AppConfig = {
   environment: 'production',
+  useJsonServerInsteadOfJavaBackend: true,
   axios: {
     baseURL: '//' + location.host + '/api',
     timeout: 30000,
