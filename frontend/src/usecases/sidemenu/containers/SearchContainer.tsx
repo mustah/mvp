2ae9152a9eb341b592e51314a4cdc6b5a0af01f3
Must Tahir <must.tahir@gmail.com>
2017-10-11 11:33:49 +0200

    Remove components from side menu that are not needed

--- frontend/src/usecases/sidemenu/containers/SearchContainer.tsx
+++
@@ -1,6 +0,0 @@
-import * as React from 'react';
-import {SearchBox} from '../components/searchbox/SearchBox';
-
-export const SearchContainer = props => (
-  <SearchBox/>
-);
--- frontend/src/usecases/sidemenu/containers/SideMenuContainer.tsx
+++ frontend/src/usecases/sidemenu/containers/SideMenuContainer.tsx
@@ -2,13 +2,9 @@ import * as React from 'react';
 import {Column} from '../../common/components/layouts/column/Column';
 import {Content} from '../../common/components/layouts/content/Content';
 import {LinkItem} from '../components/linkitem/LinkItem';
-import {NavigationContainer} from './NavigationContainer';
-import {SearchContainer} from './SearchContainer';
 
 export const SideMenuContainer = props => (
   <Column className="side-menu-container flex-1 flex-fill-horizontally">
-    <NavigationContainer/>
-    <SearchContainer/>
     <Content>
       <LinkItem name="Sparade objekt" icon="star"/>
       <LinkItem name="Sparade filter" icon="folder-star"/>
