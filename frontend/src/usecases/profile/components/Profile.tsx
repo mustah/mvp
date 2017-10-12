4ed625b70d0dbb704a1f6527fbd547c0b23a8422
Must Tahir <must.tahir@gmail.com>
2017-10-12 09:39:55 +0200

    Demonstrate Popover usage of logout

--- frontend/src/usecases/profile/components/Profile.tsx
+++ frontend/src/usecases/profile/components/Profile.tsx
@@ -1,28 +1,84 @@
+import IconButton from 'material-ui/IconButton';
+import Menu from 'material-ui/Menu';
+import MenuItem from 'material-ui/MenuItem';
+import Popover from 'material-ui/Popover/Popover';
 import * as React from 'react';
 import {User} from '../../auth/authReducer';
 import {Icon} from '../../common/components/icons/Icons';
 import {Column} from '../../common/components/layouts/column/Column';
 import {Row} from '../../common/components/layouts/row/Row';
-import {Normal} from '../../common/components/texts/Texts';
 import {MenuSeparator} from '../../topmenu/components/separators/MenuSeparator';
 import './Profile.scss';
 import {ProfileName} from './ProfileName';
 
-export interface ProfileProps {
+interface ProfileProps {
   user?: User;
   logout: () => any;
 }
 
-export const Profile = (props: ProfileProps) => {
-  const {user, logout} = props;
-  return (
-    <Column className="flex-1">
-      <Row className="Profile">
-        {user && <ProfileName user={user}/>}
-        <Icon name="account-circle"/>
-        {user && <Normal className="logout" onClick={logout}>Logout</Normal>}
-      </Row>
-      <MenuSeparator/>
-    </Column>
-  );
+interface ProfileState {
+  isOpen: boolean;
+  anchorElement?: React.ReactInstance;
+}
+
+const avatarStyle = {
+  padding: '0 0 0 10px',
+  height: '24px',
+  width: '34px',
 };
+
+export class Profile extends React.Component<ProfileProps, ProfileState> {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOpen: false,
+    };
+  }
+
+  render() {
+    const {user} = this.props;
+    const {isOpen} = this.state;
+    return (
+      <Column className="flex-1">
+        <Row className="Profile">
+          {user && <ProfileName user={user}/>}
+          <IconButton
+            disabled={!user}
+            style={avatarStyle}
+            onClick={this.openMenu}
+          >
+            <Icon name="account-circle"/>
+          </IconButton>
+          <Popover
+            open={isOpen}
+            anchorEl={this.state.anchorElement}
+            anchorOrigin={{horizontal: 'right', vertical: 'bottom'}}
+            targetOrigin={{horizontal: 'right', vertical: 'top'}}
+            onRequestClose={this.closeMenu}
+          >
+            <Menu>
+              {user && <MenuItem className="logout" onClick={this.logout}>Logout</MenuItem>}
+            </Menu>
+          </Popover>
+        </Row>
+        <MenuSeparator/>
+      </Column>
+    );
+  }
+
+  openMenu = (event: React.SyntheticEvent<any>): void => {
+    event.preventDefault();
+    this.setState({isOpen: true, anchorElement: event.currentTarget});
+  }
+
+  logout = (): void => {
+    this.closeMenu();
+    this.props.logout();
+  }
+
+  closeMenu = (): void => {
+    this.setState({isOpen: false});
+  }
+
+}
