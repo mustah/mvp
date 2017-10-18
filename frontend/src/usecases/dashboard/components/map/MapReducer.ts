fccc44513beec2a4ca1c8e92ef58c92d8c65d9a8
Daniel Svensson <dansve@elvaco.se>
2017-10-18 13:13:17 +0200

    Remove inital map points

--- frontend/src/usecases/dashboard/components/map/MapReducer.ts
+++ frontend/src/usecases/dashboard/components/map/MapReducer.ts
@@ -7,32 +7,15 @@ import {Marker} from 'leaflet';
 
 export interface MapState {
   isClusterDialogOpen: boolean;
-  markerPosition: any;
+  markerPosition?: any;
   selectedMarker?: Marker;
 }
 
 export const initialState: MapState = {
   isClusterDialogOpen: false,
-  markerPosition: [
-    {lat: 49.8397, lng: 24.0297},
-    {lat: 49.8394, lng: 24.0294},
-    {lat: 49.7394, lng: 24.0274},
-    {lat: 47.7394, lng: 23.0274},
-    {lat: 44.7394, lng: 23.0274},
-    {lat: 52.2297, lng: 21.0122},
-    {lat: 51.5074, lng: -0.0901},
-  ],
 };
 
-export const map = (state: MapState = {isClusterDialogOpen : false, markerPosition: [
-  {lat: 49.8397, lng: 24.0297},
-  {lat: 49.8394, lng: 24.0294},
-  {lat: 49.7394, lng: 24.0274},
-  {lat: 47.7394, lng: 23.0274},
-  {lat: 44.7394, lng: 23.0274},
-  {lat: 52.2297, lng: 21.0122},
-  {lat: 51.5074, lng: -0.0901},
-]}, action: AnyAction): MapState => {
+export const map = (state: MapState = {isClusterDialogOpen : false}, action: AnyAction): MapState => {
   const {payload} = action;
 
   switch (action.type) {
