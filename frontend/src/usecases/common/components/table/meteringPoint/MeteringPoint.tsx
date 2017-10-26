9b6f856f14e7b3fc6f4d0f536d40f634ac2c87e9
Carl Helmertz <carl.helmertz@elvaco.se>
2017-10-26 11:07:38 +0200

    table: Clicking a metering point cell opens a popup
    Signed-off-by: Carl Helmertz <carl.helmertz@elvaco.se>

--- frontend/src/usecases/common/components/table/meteringPoint/MeteringPoint.tsx
+++ frontend/src/usecases/common/components/table/meteringPoint/MeteringPoint.tsx
@@ -1,12 +1,61 @@
+import {FlatButton} from 'material-ui';
+import Dialog from 'material-ui/Dialog';
 import * as React from 'react';
-import {Link} from 'react-router-dom';
-import {routes} from '../../../../app/routes';
+import {translate} from '../../../../../services/translationService';
 
 interface MeteringPointProps {
   id: string;
 }
 
-export const MeteringPoint = (props: MeteringPointProps) => (
-  <div>
-    <Link to={routes.report}>{props.id}</Link>
-  </div>);
+interface MeteringPointState {
+  displayDialog: boolean;
+}
+
+export class MeteringPoint extends React.Component<MeteringPointProps, MeteringPointState> {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      displayDialog: false,
+    };
+  }
+
+  render() {
+    const {id} = this.props;
+
+    const open = (event: any) => {
+      event.preventDefault();
+      this.setState((current) => ({...current, displayDialog: true}));
+      return false;
+    };
+
+    const close = () => {
+      this.setState((current) => ({...current, displayDialog: false}));
+      return true;
+    };
+
+    const actions = [
+      (
+        <FlatButton
+          label={translate('close')}
+          primary={true}
+          onClick={close}
+        />
+      ),
+    ];
+
+    return (
+      <div>
+        <a href={'/#/meter/' + id} onClick={open}>{id}</a>
+        <Dialog
+          actions={actions}
+          open={this.state.displayDialog}
+          onRequestClose={close}
+        >
+          hej hej
+        </Dialog>
+      </div>
+    );
+  }
+}
