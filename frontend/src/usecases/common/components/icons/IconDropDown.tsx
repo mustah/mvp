d60b7f72d53bef1daaefb45d0d4b6053aba17412
Must Tahir <must.tahir@gmail.com>
2017-10-26 09:49:31 +0200

    Rename file

--- frontend/src/usecases/common/components/icons/IconDropDown.tsx
+++ frontend/src/usecases/common/components/icons/IconDropDown.tsx
@@ -2,6 +2,4 @@ import 'Icons.scss';
 import NavigationArrowDropDown from 'material-ui/svg-icons/navigation/arrow-drop-down';
 import * as React from 'react';
 
-export const IconDropDown = () => (
-  <NavigationArrowDropDown/>
-);
+export const IconDropDown = () => <NavigationArrowDropDown/>;
