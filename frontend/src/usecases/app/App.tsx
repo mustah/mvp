ab5e02b801990ed39003b6908630bd028f073f22
Must Tahir <must.tahir@gmail.com>
2017-10-27 17:47:19 +0200

    TS docs on right place

--- frontend/src/usecases/app/App.tsx
+++ frontend/src/usecases/app/App.tsx
@@ -2,7 +2,10 @@ import * as classNames from 'classnames';
 import * as React from 'react';
 import {connect} from 'react-redux';
 import {withRouter} from 'react-router';
+import {bindActionCreators} from 'redux';
 import {RootState} from '../../reducers/rootReducer';
+import {fetchGateways} from '../../state/domain-models/gateway/gatewayActions';
+import {fetchMeters} from '../../state/domain-models/meter/meterActions';
 import {isSideMenuOpen} from '../../state/ui/uiSelectors';
 import {Layout} from '../common/components/layouts/layout/Layout';
 import {Row} from '../common/components/layouts/row/Row';
@@ -11,13 +14,15 @@ import {SideMenuContainer} from '../sidemenu/containers/SideMenuContainer';
 import './_common.scss';
 import './App.scss';
 import {Pages} from './Pages';
-import {bindActionCreators} from 'redux';
-import {fetchGateways} from '../../state/domain-models/gateway/gatewayActions';
-import {fetchMeters} from '../../state/domain-models/meter/meterActions';
 
 interface StateToProps {
   isAuthenticated: boolean;
   isSideMenuOpen: boolean;
+}
+
+interface AppProps extends StateToProps {
+  fetchGateways: (filter: any) => any;
+  fetchMeters: (filter: any) => any;
 }
 
 /**
@@ -25,12 +30,6 @@ interface StateToProps {
  * for HMR (hot module reloading) to work properly. Otherwise, prefer
  * functional components.
  */
-
-interface AppProps extends StateToProps {
-  fetchGateways: (filter: any) => any;
-  fetchMeters: (filter: any) => any;
-}
-
 class App extends React.Component<AppProps> {
   componentDidMount() {
     this.props.fetchGateways([]); // TODO: Add filter instead of empty array.
