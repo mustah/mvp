9977524a1e0e0baa69969f1df96ddcf0d44e3480
Carl Helmertz <carl.helmertz@elvaco.se>
2017-11-02 10:39:05 +0100

    tabsettings: Allow user to "Export to Excel/JSON"
    Signed-off-by: Carl Helmertz <carl.helmertz@elvaco.se>

--- frontend/src/usecases/common/components/tabs/components/TabSettings.tsx
+++ frontend/src/usecases/common/components/tabs/components/TabSettings.tsx
@@ -1,3 +1,6 @@
+import Menu from 'material-ui/Menu';
+import MenuItem from 'material-ui/MenuItem';
+import Popover from 'material-ui/Popover/Popover';
 import * as React from 'react';
 import {IconMore} from '../../icons/IconMore';
 import {Column} from '../../layouts/column/Column';
@@ -8,15 +11,55 @@ export interface TabSettingsProps {
   useCase: string;
 }
 
-export const TabSettings = (props: TabSettingsProps) => {
-  const more = () => console.log('more'); // tslint:disable-line
-  // TODO Calle: open/close dropdown here, with the option "Export to Excel" to begin with
-  return (
-    <Column className="TabSettings">
-      <Row className="flex-1 Row-right clickable">
-        <IconMore onClick={more}/>
-      </Row>
-      <TabUnderline/>
-    </Column>
-  );
-};
+export interface TabSettingsState {
+  popOverOpen?: boolean;
+  popOverAnchorElement?: React.ReactInstance;
+}
+
+export class TabSettings extends React.Component<TabSettingsProps, TabSettingsState> {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      popOverOpen: false,
+    };
+  }
+
+  render() {
+    const {popOverOpen, popOverAnchorElement} = this.state;
+
+    const onClick = (event: React.SyntheticEvent<any>): void => {
+      const newState: TabSettingsState = {popOverOpen: !this.state.popOverOpen};
+      if (newState.popOverOpen) {
+        newState.popOverAnchorElement = event.currentTarget;
+      }
+      this.setState(newState);
+    };
+
+    const closePopOver = () => {
+      this.setState({popOverOpen: false});
+    };
+
+    return (
+      <Column className="TabSettings">
+        <Row className="flex-1 Row-right clickable">
+          <IconMore onClick={onClick}/>
+          <Popover
+            open={popOverOpen}
+            anchorEl={popOverAnchorElement}
+            anchorOrigin={{horizontal: 'left', vertical: 'bottom'}}
+            targetOrigin={{horizontal: 'right', vertical: 'top'}}
+            onRequestClose={closePopOver}
+          >
+            <Menu>
+              <MenuItem>Export to Excel (.csv)</MenuItem>
+              <MenuItem>Export to JSON</MenuItem>
+            </Menu>
+          </Popover>
+        </Row>
+        <TabUnderline/>
+      </Column>
+    );
+  }
+}
