19433a441a52c633557a8c1c6c9d93b48661d6b1
Carl Helmertz <carl.helmertz@elvaco.se>
2017-11-06 17:00:02 +0100

    pagination: Center, unselectable, some margin
    Signed-off-by: Carl Helmertz <carl.helmertz@elvaco.se>

--- frontend/src/usecases/common/components/pagination-control/PaginationControl.tsx
+++ frontend/src/usecases/common/components/pagination-control/PaginationControl.tsx
@@ -1,31 +1,43 @@
 import * as React from 'react';
 import {PaginationProps} from '../../../../state/ui/pagination/paginationModels';
-import {Row} from '../layouts/row/Row';
-import {Bold} from '../texts/Texts';
+import {RowCenter} from '../layouts/row/Row';
 import './PaginationControl.scss';
+import {Bold} from '../texts/Texts';
 
 export const PaginationControl = (props: PaginationProps) => {
   const {pagination: {page, limit}, changePage, numOfEntities} = props;
   const pages = Math.ceil(numOfEntities / limit);
+
   const changePageNext = () => changePage(page + 1);
   const changePagePrev = () => changePage(page - 1);
-  const prevPage = page === 1 ? null : (
-    <div onClick={changePagePrev} className="PaginationControl-button clickable">
+
+  const prevPage = page === 1 ? (
+    <div className="PaginationControl-button unselectable">
+      Previous page
+    </div>
+  ) : (
+    <div onClick={changePagePrev} className="PaginationControl-button unselectable clickable">
       Previous page
     </div>
   );
-  const nextPage = (page >= pages) ? null : (
-    <div onClick={changePageNext} className="PaginationControl-button clickable">
+
+  const nextPage = (page >= pages) ? (
+    <div className="PaginationControl-button">
+      Next page
+    </div>
+  ) : (
+    <div onClick={changePageNext} className="PaginationControl-button unselectable clickable">
       Next page
     </div>
   );
+
   return (
-    <Row className="PaginationControl">
+    <RowCenter className="PaginationControl">
       {prevPage}
       <div>
         <Bold>{page} / {pages}</Bold>
       </div>
       {nextPage}
-    </Row>
+    </RowCenter>
   );
 };
