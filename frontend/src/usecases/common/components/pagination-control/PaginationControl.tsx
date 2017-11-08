a3285efe17b130565be20c0cb2ae8f64a7594a56
Carl Helmertz <carl.helmertz@elvaco.se>
2017-11-08 09:17:01 +0100

    pagination: Render clickable, numbered links
    Demo:
    Page 1 of 10 looks like:
    1 2 3 4 ... 8 9 10
    
    Page 5 of 14 looks like:
    1 2 3 4 5 6 7 8 ... 12 13 14
    
    Page 9 of 45 looks like:
    1 2 3 ... 6 7 8 9 10 11 12 ... 43 44 45
    
    The algorithm is:
    - always show the first three pages
    - always show the last three pages
    - always show the current page +- 3 pages
    
    This supports these use cases:
    - how many pages are there?
    - how do I go to the last page?
    - how do I go to the skip "some" (3) pages from the current one?
    
    Signed-off-by: Carl Helmertz <carl.helmertz@elvaco.se>

--- frontend/src/usecases/common/components/pagination-control/PaginationControl.tsx
+++ frontend/src/usecases/common/components/pagination-control/PaginationControl.tsx
@@ -1,43 +1,61 @@
+import FlatButton from 'material-ui/FlatButton';
 import * as React from 'react';
 import {PaginationProps} from '../../../../state/ui/pagination/paginationModels';
 import {RowCenter} from '../layouts/row/Row';
 import './PaginationControl.scss';
-import {Bold} from '../texts/Texts';
 
 export const PaginationControl = (props: PaginationProps) => {
-  const {pagination: {page, limit}, changePage, numOfEntities} = props;
-  const pages = Math.ceil(numOfEntities / limit);
+    const {pagination: {page, limit}, changePage, numOfEntities} = props;
+    const pages = Math.ceil(numOfEntities / limit);
 
-  const changePageNext = () => changePage(page + 1);
-  const changePagePrev = () => changePage(page - 1);
+    /**
+     * I generally don't like "negative" variables, but I favor noPrev over hasPrev because the React component can use
+     * the variable without !inverting it, and thus not be forced to re-render as often.
+     */
+    const noPrev = page === 1;
+    const noNext = page >= pages;
 
-  const prevPage = page === 1 ? (
-    <div className="PaginationControl-button unselectable">
-      Previous page
-    </div>
-  ) : (
-    <div onClick={changePagePrev} className="PaginationControl-button unselectable clickable">
-      Previous page
-    </div>
-  );
+    const changePagePrev = noPrev ? () => void(0) : () => changePage(page - 1);
+    const changePageNext = noNext ? () => void(0) : () => changePage(page + 1);
 
-  const nextPage = (page >= pages) ? (
-    <div className="PaginationControl-button">
-      Next page
-    </div>
-  ) : (
-    <div onClick={changePageNext} className="PaginationControl-button unselectable clickable">
-      Next page
-    </div>
-  );
+    const numbers = (current: number, total: number): any[] => {
+      const pages: Array<React.ReactElement<FlatButton | HTMLSpanElement>> = [];
+      const visibilityProximity: number = 3;
+      let lastPrintedAreDots = false;
 
-  return (
-    <RowCenter className="PaginationControl">
-      {prevPage}
-      <div>
-        <Bold>{page} / {pages}</Bold>
-      </div>
-      {nextPage}
-    </RowCenter>
-  );
-};
+      // current starts with 1, meaning page should too
+      for (let page = 1; page <= total; page++) {
+        if (page === current) {
+          pages.push(<FlatButton className="PageNumber" disabled={true} key={page}>{page}</FlatButton>);
+          // pages.push(<span>{page}</span>);
+          lastPrintedAreDots = false;
+        } else if (Math.abs(page - current) <= visibilityProximity
+          || page <= visibilityProximity
+          || page > (total - visibilityProximity)) {
+          const cb = () => changePage(page);
+          pages.push(<FlatButton className="PageNumber" disabled={false} key={page} onClick={cb}>{page}</FlatButton>);
+          // pages.push(<span>{page}</span>);
+          lastPrintedAreDots = false;
+        } else if (!lastPrintedAreDots) {
+          pages.push(<span key={page}>...</span>);
+          lastPrintedAreDots = true;
+        }
+      }
+      return pages;
+    };
+
+    const renderedNumbers = numbers(page, pages);
+
+    return (
+      <RowCenter className="PaginationControl">
+        <FlatButton disabled={noPrev} onClick={changePagePrev}>
+          Föregående
+        </FlatButton>
+        {renderedNumbers}
+        <FlatButton disabled={noNext} onClick={changePageNext}>
+          Nästa
+        </FlatButton>
+      </RowCenter>
+    );
+  }
+;
