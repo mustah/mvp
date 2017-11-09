da65ec6b78bc44e8d19381fed670266ac863f361
alexander laas <laas.alexander@gmail.com>
2017-11-09 13:36:51 +0100

    Refactor signature of sidebarItems() and move types to meterModels.ts

--- frontend/src/state/domain-models/meter/meterModels.ts
+++ frontend/src/state/domain-models/meter/meterModels.ts
@@ -1,5 +1,6 @@
-import {uuid} from '../../../types/Types';
+import {IdNamed, uuid} from '../../../types/Types';
 import {MapMarker} from '../../../usecases/map/mapModels';
+import {Address} from '../domainModels';
 
 export interface Meter extends MapMarker {
   id: string;
@@ -20,4 +21,28 @@ export interface Meters {
 export interface MetersState extends Meters {
   isFetching: boolean;
   total: number;
+}
+
+type Unit = IdNamed | Address;
+
+export interface SidebarItem {
+  id: uuid;
+  name: string;
+  parent: {type: string; id: uuid};
+  selectable: boolean;
+  childNodes: {type: string; ids: uuid[]};
+}
+
+export interface SidebarItemProps {
+  unit: Unit;
+  parentType: string;
+  parent: Unit;
+  selectable: boolean;
+  childrenType: string;
+}
+
+export interface SidebarItemsProps extends SidebarItemProps {
+  sidebarTree: {[key: string]: SidebarItem[]};
+  category: string;
+  set: Set<uuid>;
 }
--- frontend/src/state/domain-models/meter/meterSelectors.ts
+++ frontend/src/state/domain-models/meter/meterSelectors.ts
@@ -1,12 +1,10 @@
-// TODO: Perhaps move this to domainSelectors
-import {Meter, MetersState} from './meterModels';
-import {createSelector} from 'reselect';
-import {getResultDomainModels} from '../domainModelsSelectors';
-import {IdNamed, uuid} from '../../../types/Types';
 import {normalize} from 'normalizr';
-import {sidebarTreeSchema} from './meterSchema';
+import {createSelector} from 'reselect';
+import {IdNamed, uuid} from '../../../types/Types';
 import {parameterNames} from '../../search/selection/selectionModels';
-import {Address} from '../domainModels';
+import {getResultDomainModels} from '../domainModelsSelectors';
+import {Meter, MetersState, SidebarItem, SidebarItemProps, SidebarItemsProps} from './meterModels';
+import {sidebarTreeSchema} from './meterSchema';
 
 export const getMetersTotal = (state: MetersState): number => state.total;
 export const getMeterEntities = (state: MetersState): {[key: string]: Meter} => state.entities.meters;
@@ -27,53 +25,42 @@ export const getSidebarTree = createSelector<MetersState, uuid[], {[key: string]
       const clusterId = city.name + ':' + clusterName;
       const cluster: IdNamed = {id: clusterId, name: clusterName};
 
-      if (!cities.has(city.id)) {
-        sidebarTree.cities.push(sidebarItem({unit: city, parentType: '', parent: {id: '', name: ''}, selectable: true, childrenType: 'addressClusters'}));
-        cities.add(city.id);
-      }
-      if (!addressClusters.has(cluster.id)) {
-        sidebarTree.addressClusters.push(
-          sidebarItem({unit: cluster, parentType: parameterNames.cities, parent: city, selectable: false, childrenType: parameterNames.addresses}),
-        );
-        sidebarTree.cities.map((cty) => {
-          if (cty.id === city.id) {
-            cty.childNodes.ids.push(cluster.id);
-            return cty;
-          } else {
-            return cty;
-          }
-        });
-        addressClusters.add(cluster.id);
-      }
-      tmp(sidebarTree, parameterNames.addresses, addresses, address, cluster, 'addressClusters', true, '');
+      sidebarItems({
+        sidebarTree,
+        category: parameterNames.cities,
+        set: cities,
+        unit: city,
+        parentType: '',
+        parent: {id: '', name: ''},
+        selectable: true,
+        childrenType: 'addressClusters',
+      });
+
+      sidebarItems({
+        sidebarTree,
+        category: 'addressClusters',
+        set: addressClusters,
+        unit: cluster,
+        parentType: parameterNames.cities,
+        parent: city,
+        selectable: false,
+        childrenType: parameterNames.addresses,
+      });
+
+      sidebarItems({
+        sidebarTree,
+        category: parameterNames.addresses,
+        set: addresses,
+        unit: address,
+        parentType: 'addressClusters',
+        parent: cluster,
+        selectable: true,
+        childrenType: '',
+      });
     });
     return normalize(sidebarTree, sidebarTreeSchema);
   },
 );
-
-type Unit = IdNamed | Address;
-
-interface SidebarItem {
-  id: uuid;
-  name: string;
-  parent: {type: string; id: uuid};
-  selectable: boolean;
-  childNodes: {type: string; ids: uuid[]};
-}
-
-interface SidebarItemProps {
-  unit: Unit;
-  parentType: string;
-  parent: Unit;
-  selectable: boolean;
-  childrenType: string;
-}
-
-interface TmpProps extends SidebarItemProps {
-  sidebarTree: {[key: string]: SidebarItem[]};
-  category: string;
-  set: Set<uuid>;
-}
 
 const sidebarItem =
   (props: SidebarItemProps): SidebarItem => {
@@ -86,17 +73,23 @@ const sidebarItem =
     };
   };
 
-const tmp = (sidebarTree: any, category: string, set: Set<uuid>, unit: Unit, parent: Unit, parentType: string, selectable: boolean, childrenType: string) => {
+const sidebarItems = (props: SidebarItemsProps) => {
+  const {sidebarTree, category, set, ...sidebarItemProps} = props;
+  const {unit, parent, parentType} = props;
   if (!set.has(unit.id)) {
-    sidebarTree[category].push(sidebarItem({unit, parentType, parent, selectable, childrenType}));
-    sidebarTree[parentType].map((par) => {
-      if (par.id === parent.id) {
-        par.childNodes.ids.push(unit.id);
-        return par;
-      } else {
-        return par;
-      }
-    });
+
+    sidebarTree[category].push(sidebarItem(sidebarItemProps));
     set.add(unit.id);
+
+    if (parentType !== '') {
+      sidebarTree[parentType].map((par) => {
+        if (par.id === parent.id) {
+          par.childNodes.ids.push(unit.id);
+          return par;
+        } else {
+          return par;
+        }
+      });
+    }
   }
 };
