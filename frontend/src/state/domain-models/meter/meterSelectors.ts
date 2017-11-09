9749f6cfd73d3d4daeb4393a8f202d99ca0c63b9
alexander laas <laas.alexander@gmail.com>
2017-11-09 11:48:17 +0100

    Add typings to getSidebarTree()

--- frontend/src/state/domain-models/meter/meterSelectors.ts
+++ frontend/src/state/domain-models/meter/meterSelectors.ts
@@ -1,51 +1,54 @@
-
 // TODO: Perhaps move this to domainSelectors
 import {Meter, MetersState} from './meterModels';
 import {createSelector} from 'reselect';
 import {getResultDomainModels} from '../domainModelsSelectors';
-import {uuid} from '../../../types/Types';
+import {IdNamed, uuid} from '../../../types/Types';
 import {normalize} from 'normalizr';
 import {sidebarTreeSchema} from './meterSchema';
 import {parameterNames} from '../../search/selection/selectionModels';
+import {Address} from '../domainModels';
 
 export const getMetersTotal = (state: MetersState): number => state.total;
 export const getMeterEntities = (state: MetersState): {[key: string]: Meter} => state.entities.meters;
 
 export const getSidebarTree = createSelector<MetersState, uuid[], {[key: string]: Meter}, any>(
-    getResultDomainModels,
-    getMeterEntities,
-    (metersList: uuid[], meters: {[key: string]: Meter}) => {
+  getResultDomainModels,
+  getMeterEntities,
+  (metersList: uuid[], meters: {[key: string]: Meter}) => {
 
-      const sidebarTree: {[key: string]: any[]} = {cities: [], addresses: [], addressClusters: []};
-      const cities = new Set();
-      const addressClusters = new Set();
-      const addresses = new Set();
+    const sidebarTree: {[key: string]: SidebarItem[]} = {cities: [], addresses: [], addressClusters: []};
+    const cities = new Set<uuid>();
+    const addressClusters = new Set<uuid>();
+    const addresses = new Set<uuid>();
 
-      metersList.map((meterId: uuid) => {
+    metersList.map((meterId: uuid) => {
       const {city, address} = meters[meterId];
-      if (!cities.has(city)) {
+      const clusterName = address.name[0];
+      const clusterId = city.name + ':' + clusterName;
+      const cluster: IdNamed = {id: clusterId, name: clusterName};
+
+      if (!cities.has(city.id)) {
         sidebarTree.cities.push(sidebarItem(city.id, city.name, '', '', true, 'addressClusters'));
-        cities.add(city);
+        cities.add(city.id);
       }
-      const clusterId = city.name + ':' + address.name[0];
-      if (!addressClusters.has(clusterId)) {
+      if (!addressClusters.has(cluster.id)) {
         sidebarTree.addressClusters.push(
-          sidebarItem(clusterId, address.name[0], parameterNames.cities, city.id, false, parameterNames.addresses),
+          sidebarItem(cluster.id, cluster.name, parameterNames.cities, city.id, false, parameterNames.addresses),
         );
         sidebarTree.cities.map((cty) => {
           if (cty.id === city.id) {
-            cty.childNodes.ids.push(clusterId);
+            cty.childNodes.ids.push(cluster.id);
             return cty;
           } else {
             return cty;
           }
         });
-        addressClusters.add(clusterId);
+        addressClusters.add(cluster.id);
       }
       if (!addresses.has(address.id)) {
-        sidebarTree.addresses.push(sidebarItem(address.id, address.name, 'addressClusters', clusterId, true, ''));
+        sidebarTree.addresses.push(sidebarItem(address.id, address.name, 'addressClusters', cluster.id, true, ''));
         sidebarTree.addressClusters.map((cl) => {
-          if (cl.id === clusterId) {
+          if (cl.id === cluster.id) {
             cl.childNodes.ids.push(address.id);
             return cl;
           } else {
@@ -55,11 +58,34 @@ export const getSidebarTree = createSelector<MetersState, uuid[], {[key: string]
         addresses.add(address.id);
       }
 
-      });
-      return normalize(sidebarTree, sidebarTreeSchema);
-      },
-  );
+    });
+    return normalize(sidebarTree, sidebarTreeSchema);
+  },
+);
+
+interface SidebarItem {
+  id: uuid;
+  name: string;
+  parent: {type: string; id: uuid};
+  selectable: boolean;
+  childNodes: {type: string; ids: uuid[]};
+}
 
 const sidebarItem =
-  (id: uuid, name: string, pType: string, pId: uuid, selectable: boolean, cType: string) =>
+  (id: uuid, name: string, pType: string, pId: uuid, selectable: boolean, cType: string): SidebarItem =>
     ({id, name, parent: {type: pType, id: pId}, selectable, childNodes: {type: cType, ids: []}});
+
+const tmp = (parents: SidebarItem[], siblings: SidebarItem[], set: Set<uuid>, unit: IdNamed | Address, parent: IdNamed | Address, parentType: string, selectable: boolean, childType: string) => {
+  if (!set.has(unit.id)) {
+    siblings.push(sidebarItem(unit.id, unit.name, parentType, parent.id, selectable, childType));
+    parents.map((un) => {
+      if (un.id === parent.id) {
+        un.childNodes.ids.push(unit.id);
+        return un;
+      } else {
+        return un;
+      }
+    });
+    set.add(unit.id);
+  }
+};
