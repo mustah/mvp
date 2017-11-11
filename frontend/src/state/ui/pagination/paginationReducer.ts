33ddfd327170715d913d5a2cb1db440536dc89d1
Must Tahir <must.tahir@gmail.com>
2017-11-11 23:03:55 +0100

    Show 30 items per page and minor styling of inline edit

--- frontend/src/state/ui/pagination/paginationReducer.ts
+++ frontend/src/state/ui/pagination/paginationReducer.ts
@@ -1,16 +1,19 @@
 import {AnyAction} from 'redux';
-import {PaginationState} from './paginationModels';
 import {PAGINATION_CHANGE_PAGE} from './paginationActions';
+import {PaginationState} from './paginationModels';
+
+const limit = 30;
 
 const initialState: PaginationState = {
-  dashboard: {page: 1, limit: 20},
-  collection: {page: 1, limit: 20},
-  validation: {page: 1, limit: 20},
-  selection: {page: 1, limit: 10},
+  dashboard: {page: 1, limit},
+  collection: {page: 1, limit},
+  validation: {page: 1, limit},
+  selection: {page: 1, limit},
 };
 
 export const pagination = (state: PaginationState = initialState, action: AnyAction) => {
   const {payload} = action;
+
   switch (action.type) {
     case PAGINATION_CHANGE_PAGE:
       return {
