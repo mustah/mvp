545e2e47b780227c7c97e8549397a89ad117a593
Carl Helmertz <carl.helmertz@elvaco.se>
2017-11-16 15:22:05 +0100

    header: Move <Profile> to top right, and <Logo> to center
    Also: fix the bug that made you logout instead of close the "profile
    menu".
    
    Signed-off-by: Carl Helmertz <carl.helmertz@elvaco.se>

--- frontend/src/usecases/profile/components/IconAvatar.tsx
+++ frontend/src/usecases/profile/components/IconAvatar.tsx
@@ -2,6 +2,7 @@ import IconButton from 'material-ui/IconButton';
 import ActionAccountCircle from 'material-ui/svg-icons/action/account-circle';
 import * as React from 'react';
 import {Clickable} from '../../../types/Types';
+import {colors} from '../../app/themes';
 
 const avatarStyle = {
   padding: 0,
@@ -13,7 +14,7 @@ export const Avatar = (props: Clickable) => {
   const {onClick} = props;
   return (
     <IconButton style={avatarStyle} onClick={onClick}>
-      <ActionAccountCircle color="white"/>
+      <ActionAccountCircle color={colors.darkBlue}/>
     </IconButton>
   );
 };
