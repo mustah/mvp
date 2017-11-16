5762adeeef8a9a22e3b6dd8cf2137a7fb67df705
Must Tahir <must.tahir@gmail.com>
2017-11-16 17:42:39 +0100

    Add more space below the period dropdown

--- frontend/src/usecases/common/components/dates/PeriodSelection.tsx
+++ frontend/src/usecases/common/components/dates/PeriodSelection.tsx
@@ -17,7 +17,7 @@ const style: React.CSSProperties = {
   border: `1px solid ${colors.borderColor}`,
   borderRadius: 3,
   marginLeft: 24,
-  marginBottom: 8,
+  marginBottom: 16,
 };
 
 const underlineStyle: React.CSSProperties = {
