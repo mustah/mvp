acc80e1ce26188c06fddde2d60a59c3aa2373681
Must Tahir <must.tahir@gmail.com>
2017-11-16 15:28:27 +0100

    Navigate to report page from a list item action

--- frontend/src/usecases/app/Pages.tsx
+++ frontend/src/usecases/app/Pages.tsx
@@ -24,7 +24,7 @@ export const Pages = (props) => (
     <Route exact={true} path={routes.dashboard} component={DashboardPage}/>
     <Route exact={true} path={routes.collection} component={CollectionPage}/>
     <Route exact={true} path={routes.validation} component={ValidationPage}/>
-    <Route exact={true} path={routes.report} component={ReportPage}/>
+    <Route exact={false} path={routes.report} component={ReportPage}/>
     <Route exact={true} path="/:page/search" component={SearchPage}/>
     <Route exact={true} path="/search" component={SearchPage}/>
   </Layout>
--- frontend/src/usecases/common/components/actions-dropdown/ListActionsDropdown.tsx
+++ frontend/src/usecases/common/components/actions-dropdown/ListActionsDropdown.tsx
@@ -2,6 +2,8 @@ import * as React from 'react';
 import {translate} from '../../../../services/translationService';
 import {IdNamed} from '../../../../types/Types';
 import {ActionsDropdown, CallbackAction} from './ActionsDropdown';
+import {history} from '../../../../index';
+import {routes} from '../../../app/routes';
 
 interface Props {
   item: IdNamed;
@@ -17,7 +19,7 @@ export const ListActionsDropdown = (props: Props) => {
     {name: translate('export to JSON'), onClick: noop},
     {
       name: translate('add to report'),
-      onClick: () => item,
+      onClick: () => history.push(`${routes.report}/${item.id}`),
     },
   ];
 
