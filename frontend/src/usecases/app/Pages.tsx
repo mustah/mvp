98104544976a047503df835e87bff5e983d0fabb
Must Tahir <must.tahir@gmail.com>
2017-11-16 15:42:37 +0100

    Use better page routing

--- frontend/src/usecases/app/Pages.tsx
+++ frontend/src/usecases/app/Pages.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {Route} from 'react-router';
+import {Route, Switch} from 'react-router';
 import {userIsAuthenticated, userIsNotAuthenticated} from '../../services/authService';
 import {LoginContainer} from '../auth/containers/LoginContainer';
 import {Collection} from '../collection/components/Collection';
@@ -7,8 +7,8 @@ import {Layout} from '../common/components/layouts/layout/Layout';
 import DashboardContainer from '../dashboard/containers/DashboardContainer';
 import ReportContainer from '../report/containers/ReportContainer';
 import {SelectionContainer} from '../selection/containers/SelectionContainer';
-import {routes} from './routes';
 import {Validation} from '../validation/components/Validation';
+import {routes} from './routes';
 
 const LoginPage = userIsNotAuthenticated(LoginContainer);
 const DashboardPage = userIsAuthenticated(DashboardContainer);
@@ -19,13 +19,17 @@ const SearchPage = userIsAuthenticated(SelectionContainer);
 
 export const Pages = (props) => (
   <Layout className="flex-1">
-    <Route path={routes.login} component={LoginPage}/>
-    <Route exact={true} path={routes.home} component={DashboardPage}/>
-    <Route exact={true} path={routes.dashboard} component={DashboardPage}/>
-    <Route exact={true} path={routes.collection} component={CollectionPage}/>
-    <Route exact={true} path={routes.validation} component={ValidationPage}/>
-    <Route exact={false} path={routes.report} component={ReportPage}/>
-    <Route exact={true} path="/:page/search" component={SearchPage}/>
-    <Route exact={true} path="/search" component={SearchPage}/>
+    <Switch>
+      <Route path={routes.login} component={LoginPage}/>
+      <Route exact={true} path="/:page/search" component={SearchPage}/>
+      <Route exact={true} path="/:page/:id/search" component={SearchPage}/>
+      <Route exact={true} path={routes.home} component={DashboardPage}/>
+      <Route exact={true} path={routes.dashboard} component={DashboardPage}/>
+      <Route exact={true} path={routes.collection} component={CollectionPage}/>
+      <Route exact={true} path={routes.validation} component={ValidationPage}/>
+      <Route exact={true} path={`${routes.report}/:id`} component={ReportPage}/>
+      <Route exact={true} path={routes.report} component={ReportPage}/>
+       <Route exact={true} path="/search" component={SearchPage}/>
+    </Switch>
   </Layout>
 );
