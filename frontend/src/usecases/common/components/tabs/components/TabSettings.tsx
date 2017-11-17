6bdcfa1f42ab0743c5192cf848c2ecb4c8d779c1
Carl Helmertz <carl.helmertz@elvaco.se>
2017-11-17 17:45:06 +0100

    actions: Draw a divider between "export to.." and "show in report"
    Signed-off-by: Carl Helmertz <carl.helmertz@elvaco.se>

--- frontend/src/usecases/common/components/tabs/components/TabSettings.tsx
+++ frontend/src/usecases/common/components/tabs/components/TabSettings.tsx
@@ -1,20 +1,20 @@
 import * as React from 'react';
 import {translate} from '../../../../../services/translationService';
-import {ActionsDropdown, CallbackAction} from '../../actions-dropdown/ActionsDropdown';
+import {ActionsDropdown, menuItem, MenuItems} from '../../actions-dropdown/ActionsDropdown';
 import {Column} from '../../layouts/column/Column';
 import {TabUnderline} from './TabUnderliner';
 
 export const TabSettings = () => {
   const noop = () => 0;
 
-  const actions: CallbackAction[] = [
+  const menuItems: MenuItems = [
     {name: translate('export to Excel (.csv)'), onClick: noop},
     {name: translate('export to JSON'), onClick: noop},
-  ];
+  ].map(menuItem);
 
   return (
     <Column className="TabSettings">
-      <ActionsDropdown actions={actions} className="flex-1 Row-right"/>
+      <ActionsDropdown menuItems={menuItems} className="flex-1 Row-right"/>
       <TabUnderline/>
     </Column>
   );
