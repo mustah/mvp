819dde133386bfe4c57af38252fb1508fe0e9baf
Must Tahir <must.tahir@gmail.com>
2017-11-17 15:05:33 +0100

    Easier page navigations

--- frontend/src/usecases/app/Pages.tsx
+++ frontend/src/usecases/app/Pages.tsx
@@ -21,15 +21,13 @@ export const Pages = (props) => (
   <Layout className="flex-1">
     <Switch>
       <Route path={routes.login} component={LoginPage}/>
-      <Route exact={true} path="/:page/selection" component={SelectionPage}/>
-      <Route exact={true} path="/:page/:id/selection" component={SelectionPage}/>
       <Route exact={true} path={routes.home} component={DashboardPage}/>
       <Route exact={true} path={routes.dashboard} component={DashboardPage}/>
       <Route exact={true} path={routes.collection} component={CollectionPage}/>
       <Route exact={true} path={routes.validation} component={ValidationPage}/>
       <Route exact={true} path={`${routes.report}/:id`} component={ReportPage}/>
       <Route exact={true} path={routes.report} component={ReportPage}/>
-      <Route exact={true} path="/selection" component={SelectionPage}/>
+      <Route exact={true} path={routes.selection} component={SelectionPage}/>
       <Redirect to={routes.home}/>
     </Switch>
   </Layout>
