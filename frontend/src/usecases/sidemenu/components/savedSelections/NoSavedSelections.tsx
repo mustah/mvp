1dc1598c3a0b087012ab139698b7555188671c95
Carl Helmertz <carl.helmertz@elvaco.se>
2017-11-20 11:54:32 +0100

    savedSelections: Correctly uppercase "no saved selections"
    Signed-off-by: Carl Helmertz <carl.helmertz@elvaco.se>

--- frontend/src/usecases/sidemenu/components/savedSelections/NoSavedSelections.tsx
+++ frontend/src/usecases/sidemenu/components/savedSelections/NoSavedSelections.tsx
@@ -5,7 +5,7 @@ import {Row} from '../../../common/components/layouts/row/Row';
 import {Normal} from '../../../common/components/texts/Texts';
 
 export const NoSavedSelections = () => (
-  <Row className="NoSavedSelections first-uppercase">
-    <Normal className="Italic">{translate('no saved selections')}</Normal>
+  <Row className="NoSavedSelections">
+    <Normal className="Italic first-uppercase">{translate('no saved selections')}</Normal>
   </Row>
 );
