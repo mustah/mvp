7d8fa57e13a8afcd847c325472601a5324a7b042
Anton Lofgren <antlof@elvaco.se>
2017-11-20 10:08:06 +0100

    Fix broken measurement query
    We need to select the physical_meter_id here, or the entity mapping will
    go nuts.

--- backend/src/main/java/com/elvaco/mvp/repository/MeasurementRepository.java
+++ backend/src/main/java/com/elvaco/mvp/repository/MeasurementRepository.java
@@ -9,7 +9,7 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 public interface MeasurementRepository extends PagingAndSortingRepository<MeasurementEntity, Long> {
   Page<MeasurementEntity> findByQuantity(String quantity, Pageable pageable);
 
-  @Query(value = "SELECT id, created, quantity, unit_at(value, ?2) as value FROM measurement WHERE quantity = ?1 ORDER BY ?#{#pageable}",
+  @Query(value = "SELECT id, physical_meter_id, created, quantity, unit_at(value, ?2) as value FROM measurement WHERE quantity = ?1 ORDER BY ?#{#pageable}",
     countQuery = "SELECT count(id) FROM measurement WHERE quantity = ?1 ORDER BY ?#{#pageable}",
     nativeQuery = true)
   Page<MeasurementEntity> findByQuantityScaled(String quantity, String scaleDimension, Pageable pageable);
