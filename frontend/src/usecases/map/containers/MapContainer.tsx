2ec3d421fcbe1e2b54eae5e468a42d0f369c75d2
Daniel Svensson <dansve@elvaco.se>
2017-11-27 11:20:06 +0100

    Break up map logic into functions

--- frontend/src/usecases/map/containers/MapContainer.tsx
+++ frontend/src/usecases/map/containers/MapContainer.tsx
@@ -20,6 +20,11 @@ import {openClusterDialog, toggleClusterDialog} from '../mapActions';
 import {ExtendedMarker, MapMarker} from '../mapModels';
 import {MapState} from '../mapReducer';
 import './MapContainer.scss';
+import {isNullOrUndefined} from 'util';
+import {Gateway} from '../../../state/domain-models/gateway/gatewayModels';
+import {Meter} from '../../../state/domain-models/meter/meterModels';
+import {GeoPosition} from '../../../state/domain-models/domainModels';
+import {IdNamed} from '../../../types/Types';
 
 interface StateToProps {
   map: MapState;
@@ -49,8 +54,10 @@ export const enum PopupMode {
   none,
 }
 
-class MapContainer extends React.Component<StateToProps & DispatchToProps & OwnProps> {
+const maxZoom = 18;
+const minZoom = 3;
 
+class MapContainer extends React.Component<StateToProps & DispatchToProps & OwnProps, any> {
   render() {
     const {
       map,
@@ -70,121 +77,68 @@ class MapContainer extends React.Component<StateToProps & DispatchToProps & OwnP
       onMarkerClick = () => void(0);
     }
 
-    let tmpMarkers: DomainModel<MapMarker> = {};
+    let tmpMarkers: { [key: string]: MapMarker } = {};
     if (isMapMarker(markers)) {
       tmpMarkers[0] = markers;
     } else {
       tmpMarkers = markers;
     }
 
-    const maxZoom = 18;
-    const minZoom = 3;
-
     const markerclusterOptions = {
       // Setting custom icon for cluster group
-      iconCreateFunction: (cluster) => {
-        // TODO Test performance!
-        // TODO Find status of the marker instead of guessing by checking iconUrl
-        // Set cluster css class depending on underlying marker icons
-        let cssClass = '';
-        let errorCount = 0;
-        let warningCount = 0;
-        for (const child of cluster.getAllChildMarkers()) {
-          if (child.options.icon.options.iconUrl === 'assets/images/marker-icon-error.png') {
-            errorCount++;
-          } else if (child.options.icon.options.iconUrl === 'assets/images/marker-icon-warning.png') {
-            warningCount++;
-          }
-        }
 
-        let x = cluster.getChildCount() / 9;
-        if (x > 90) {
-          x = 100;
-        } else if (x < 30) {
-          x = 30;
-        }
-
-        let percent = (cluster.getChildCount() - errorCount - warningCount) / cluster.getChildCount() * 100;
-        percent = Math.floor(percent);
-
-        if (percent === 100) {
-          cssClass = 'marker-cluster-ok';
-        } else if (percent > 90) {
-          cssClass = 'marker-cluster-warning';
-        } else {
-          cssClass = 'marker-cluster-error';
-        }
-
-        return L.divIcon({
-          html: `<span>${cluster.getChildCount()}</span>`,
-          className: cssClass,
-          iconSize: L.point(x, x, true),
-        });
-      },
+      iconCreateFunction: handleIconCreate,
       chunkedLoading: true,
       showCoverageOnHover: true,
-      maxClusterRadius: (currentZoom) => {
-        if (currentZoom < maxZoom) {
-          return 80;
-        } else {
-          return 5;
-        }
-      },
+      maxClusterRadius: getZoomBasedRadius,
     };
 
     const confidenceThreshold: number = 0.7;
     // TODO type array
     const leafletMarkers: any[] = [];
-    let tmpIcon;
 
     // TODO break up marker icon logic into methods and add tests
     if (tmpMarkers) {
       Object.keys(tmpMarkers).forEach((key: string) => {
         const marker = tmpMarkers[key];
-
-        // TODO This logic is currently very fragile. We don't know every possible status, and how severe that status is
-        switch (marker.status.id) {
-          case 0:
-          case 1:
-            tmpIcon = 'assets/images/marker-icon-ok.png';
-            break;
-          case 2:
-            tmpIcon = 'assets/images/marker-icon-warning.png';
-            break;
-          case 3:
-            tmpIcon = 'assets/images/marker-icon-error.png';
-            break;
-          default:
-            tmpIcon = 'assets/images/marker-icon.png';
-            break;
-        }
-
         const {latitude, longitude, confidence} = marker.position;
 
         if (latitude && longitude && confidence >= confidenceThreshold) {
           leafletMarkers.push({
-              lat: latitude,
-              lng: longitude,
-              options: {
-                icon: L.icon({
-                  iconUrl: tmpIcon,
-                }),
-                mapMarker: marker,
-              },
-              status,
+            lat: latitude,
+            lng: longitude,
+            options: {
+              icon: L.icon({
+                iconUrl: getIcon(marker.status),
+              }),
+              mapMarker: marker,
             },
-          );
+          });
         }
       });
     }
 
-    const toggleScrollWheelZoom = (e) => {
-      if (e.target.scrollWheelZoom.enabled()) {
-        e.target.scrollWheelZoom.disable();
-      } else {
-        e.target.scrollWheelZoom.enable();
+    let popup;
+
+    if (!isNullOrUndefined(map.selectedMarker) && map.selectedMarker.options) {
+      if (popupMode === PopupMode.gateway) {
+        popup = map.isClusterDialogOpen && (
+          <GatewayDialogContainer
+            gateway={map.selectedMarker.options.mapMarker as Gateway}
+            displayDialog={map.isClusterDialogOpen}
+            close={toggleClusterDialog}
+          />
+        );
+      } else if (popupMode === PopupMode.meterpoint) {
+        popup = map.isClusterDialogOpen && (
+          <MeteringPointDialogContainer
+            meter={map.selectedMarker.options.mapMarker as Meter}
+            displayDialog={map.isClusterDialogOpen}
+            close={toggleClusterDialog}
+          />
+        );
       }
-    };
+    }
 
     const renderCluster = () => leafletMarkers.length > 0 && (
       <MarkerClusterGroup
@@ -238,6 +192,98 @@ class MapContainer extends React.Component<StateToProps & DispatchToProps & OwnP
   }
 
 }
+
+const getZoomBasedRadius = (zoom) => {
+  if (zoom < maxZoom) {
+    return 80;
+  } else {
+    return 5;
+  }
+}
+
+const handleIconCreate = (cluster: MarkerClusterGroup) => {
+  let x = getCLusterDimensions(cluster.getChildCount());
+
+  return L.divIcon({
+    html: `<span>${cluster.getChildCount()}</span>`,
+    className: getClusterCssClass(cluster),
+    iconSize: L.point(x, x, true),
+  });
+}
+
+const getClusterCssClass = (cluster: MarkerClusterGroup): string => {
+  // TODO Test performance!
+  // TODO Find status of the marker instead of guessing by checking iconUrl
+  // Set cluster css class depending on underlying marker icons
+
+  let errorCount = 0;
+  let warningCount = 0;
+  for (const child of cluster.getAllChildMarkers()) {
+    if (child.options.icon.options.iconUrl === 'assets/images/marker-icon-error.png') {
+      errorCount++;
+    } else if (child.options.icon.options.iconUrl === 'assets/images/marker-icon-warning.png') {
+      warningCount++;
+    }
+  }
+
+  let percent = (cluster.getChildCount() - errorCount - warningCount) / cluster.getChildCount() * 100;
+  percent = Math.floor(percent);
+
+  let cssClass: string;
+  if (percent === 100) {
+    cssClass = 'marker-cluster-ok';
+  } else if (percent > 90) {
+    cssClass = 'marker-cluster-warning';
+  } else {
+    cssClass = 'marker-cluster-error';
+  }
+
+  return cssClass;
+}
+
+const getCLusterDimensions = (clusterCount: number): number => {
+  console.log('clusterCount ' + clusterCount );
+  let x = clusterCount / 9;
+
+  if (x > 90) {
+    x = 100;
+  } else if (x < 30) {
+    x = 30;
+  }
+
+  return x;
+}
+
+const getIcon = (status: IdNamed): string => {
+  let tmpIcon: string;
+
+  // TODO This logic is currently very fragile. We don't know every possible status, and how severe that status is.
+  switch (status.id) {
+    case 0:
+    case 1:
+      tmpIcon = 'assets/images/marker-icon-ok.png';
+      break;
+    case 2:
+      tmpIcon = 'assets/images/marker-icon-warning.png';
+      break;
+    case 3:
+      tmpIcon = 'assets/images/marker-icon-error.png';
+      break;
+    default:
+      tmpIcon = 'assets/images/marker-icon.png';
+      break;
+  }
+
+  return tmpIcon;
+}
+
+const toggleScrollWheelZoom = (e) => {
+  if (e.target.scrollWheelZoom.enabled()) {
+    e.target.scrollWheelZoom.disable();
+  } else {
+    e.target.scrollWheelZoom.enable();
+  }
+};
 
 const isMapMarker = (obj: any): obj is MapMarker => {
   return obj && obj.status !== undefined && obj.position !== undefined;
