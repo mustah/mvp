fd87e16bdad9a3f975161e7032b3fb5844cbeca2
Anton Lofgren <antlof@elvaco.se>
2017-12-01 14:40:19 +0100

    Make `test` source set conform to the GJSG

--- backend/src/test/java/com/elvaco/mvp/api/ApiExceptionHandlerTest.java
+++ backend/src/test/java/com/elvaco/mvp/api/ApiExceptionHandlerTest.java
@@ -1,17 +1,18 @@
 package com.elvaco.mvp.api;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import com.elvaco.mvp.dto.ErrorMessageDto;
 import org.junit.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 public class ApiExceptionHandlerTest {
 
   @Test
   public void checkException() throws Exception {
-    ResponseEntity<ErrorMessageDto> response = new ApiExceptionHandler().handle(new Exception("test"));
+    ResponseEntity<ErrorMessageDto> response =
+        new ApiExceptionHandler().handle(new Exception("test"));
 
     assertThat(response.getStatusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR);
     assertThat(response.getBody().message).isEqualTo("test");
--- backend/src/test/java/com/elvaco/mvp/entity/measurement/MeasurementUnitTest.java
+++ backend/src/test/java/com/elvaco/mvp/entity/measurement/MeasurementUnitTest.java
@@ -1,8 +1,8 @@
 package com.elvaco.mvp.entity.measurement;
 
-import org.junit.Test;
-
 import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
 
 public class MeasurementUnitTest {
   @Test
