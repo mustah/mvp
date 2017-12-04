6d6d17b66747cb9e312852a87e706d08065552e9
Carl Helmertz <carl.helmertz@elvaco.se>
2017-12-04 15:56:37 +0100

    test: Pagination selectors
    Signed-off-by: Carl Helmertz <carl.helmertz@elvaco.se>

--- frontend/src/state/ui/pagination/paginationSelectors.ts
+++ frontend/src/state/ui/pagination/paginationSelectors.ts
@@ -1,17 +1,18 @@
-import {createSelector} from 'reselect';
+import {createSelector, OutputSelector} from 'reselect';
 import {useCases} from '../../../types/constants';
 import {uuid} from '../../../types/Types';
 import {UiState} from '../uiReducer';
 import {Pagination, PaginationState} from './paginationModels';
 
-interface PaginatedDomainModel {
+export interface PaginatedDomainModel {
   pagination: Pagination;
   result: uuid[];
 }
 
 const getPaginationState = (state: UiState): PaginationState => state.pagination;
 
-const getPaginationFor = (useCase: string): any =>
+type UseCasePaginationSelector = OutputSelector<UiState, Pagination, (res: PaginationState) => Pagination>;
+const getPaginationFor: (useCase: string) => UseCasePaginationSelector = (useCase: string) =>
   createSelector<UiState, PaginationState, Pagination>(
     getPaginationState,
     (pagination: PaginationState) => pagination[useCase],
@@ -20,13 +21,15 @@ const getPaginationFor = (useCase: string): any =>
 const getResult = (state: PaginatedDomainModel): uuid[] => state.result;
 const getPagination = (state: PaginatedDomainModel): Pagination => state.pagination;
 
-export const getPaginationList = createSelector<PaginatedDomainModel, uuid[], Pagination, uuid[]>(
-  getResult,
-  getPagination,
-  (result: uuid[], {page, limit}: Pagination) => {
-    return result.slice((page - 1) * limit, page * limit);
-  });
+type PaginationListSelector = OutputSelector<PaginatedDomainModel, uuid[], (res1: uuid[], res2: Pagination) => uuid[]>;
+export const getPaginationList: PaginationListSelector =
+  createSelector<PaginatedDomainModel, uuid[], Pagination, uuid[]>(
+    getResult,
+    getPagination,
+    (result: uuid[], {page, limit}: Pagination) => {
+      return result.slice((page - 1) * limit, page * limit);
+    });
 
-export const getCollectionPagination = getPaginationFor(useCases.collection);
-export const getValidationPagination = getPaginationFor(useCases.validation);
-export const getSelectionPagination = getPaginationFor(useCases.selection);
+export const getCollectionPagination: UseCasePaginationSelector = getPaginationFor(useCases.collection);
+export const getValidationPagination: UseCasePaginationSelector = getPaginationFor(useCases.validation);
+export const getSelectionPagination: UseCasePaginationSelector = getPaginationFor(useCases.selection);
