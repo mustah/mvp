606332b88690d28d0258843e8ec70f87869d1cf3
Carl Helmertz <carl.helmertz@elvaco.se>
2017-12-05 16:26:39 +0100

    indicatorSelector: Test getSelectedIndicatorReport
    Signed-off-by: Carl Helmertz <carl.helmertz@elvaco.se>

--- frontend/src/state/ui/indicator/indicatorSelectors.ts
+++ frontend/src/state/ui/indicator/indicatorSelectors.ts
@@ -1,11 +1,12 @@
-import {createSelector} from 'reselect';
+import {createSelector, OutputSelector} from 'reselect';
 import {IndicatorType} from '../../../components/indicators/models/widgetModels';
 import {UiState} from '../uiReducer';
 import {IndicatorState} from './indicatorReducer';
 
 const getIndicatorState = (state: UiState): IndicatorState => state.indicator;
 
-const getSelectedIndicator = (useCase: string): any =>
+type SelectedIndicatorSelector = OutputSelector<UiState, IndicatorType, (res: IndicatorState) => IndicatorType>;
+const getSelectedIndicator = (useCase: string): SelectedIndicatorSelector =>
   createSelector<UiState, IndicatorState, IndicatorType>(
     getIndicatorState,
     (indicator: IndicatorState) => indicator.selectedIndicators[useCase],
