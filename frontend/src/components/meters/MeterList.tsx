5a969d695718830eb40a8fa372ddfcc6a04b03a5
Must Tahir <must.tahir@gmail.com>
2017-12-06 09:42:02 +0100

    Use object destructuring in MeterList.tsx

--- frontend/src/components/meters/MeterList.tsx
+++ frontend/src/components/meters/MeterList.tsx
@@ -1,14 +1,15 @@
 import * as React from 'react';
 import {translate} from '../../services/translationService';
+import {Normalized} from '../../state/domain-models/domainModels';
+import {Flag} from '../../state/domain-models/flag/flagModels';
 import {Meter} from '../../state/domain-models/meter/meterModels';
+import {OnClickWithId} from '../../types/Types';
+import {ListActionsDropdown} from '../actions-dropdown/ListActionsDropdown';
 import {Separator} from '../separators/Separator';
 import {Status} from '../status/Status';
 import {Table, TableColumn} from '../table/Table';
 import {TableHead} from '../table/TableHead';
 import {MeterListItem} from './MeterListItem';
-import {ListActionsDropdown} from '../actions-dropdown/ListActionsDropdown';
-import {OnClickWithId} from '../../types/Types';
-import {Normalized} from '../../state/domain-models/domainModels';
 
 interface Props {
   selectEntryAdd: OnClickWithId;
@@ -18,16 +19,16 @@ export const MeterList = (props: Normalized<Meter> & Props) => {
   const {result, entities, selectEntryAdd} = props;
 
   const renderMeterListItem = (meter: Meter) => <MeterListItem meter={meter}/>;
-  const renderStatusCell = (meter: Meter) => <Status {...meter.status}/>;
-  const renderCityName = (meter: Meter) => meter.city.name;
-  const renderAddressName = (meter: Meter) => meter.address.name;
-  const renderFlags = (meter: Meter) => meter.flags.map((flag) => flag.title).join(', ');
-  const renderActionDropdown = (meter: Meter) =>
-    <ListActionsDropdown item={{id: meter.id, name: meter.manufacturer}} selectEntryAdd={selectEntryAdd}/>;
-  const renderGatewayId = (meter: Meter) => meter.gatewayId;
-  const renderManufacturer = (meter: Meter) => meter.manufacturer;
-  const renderStatusChanged = (meter: Meter) => meter.statusChanged || <Separator/>;
-  const renderMedium = (meter: Meter) => meter.medium;
+  const renderStatusCell = ({status}: Meter) => <Status {...status}/>;
+  const renderCityName = ({city}: Meter) => city.name;
+  const renderAddressName = ({address}: Meter) => address.name;
+  const renderFlags = ({flags}: Meter) => flags.map((flag: Flag) => flag.title).join(', ');
+  const renderActionDropdown = ({id, manufacturer}: Meter) =>
+    <ListActionsDropdown item={{id, name: manufacturer}} selectEntryAdd={selectEntryAdd}/>;
+  const renderGatewayId = ({gatewayId}: Meter) => gatewayId;
+  const renderManufacturer = ({manufacturer}: Meter) => manufacturer;
+  const renderStatusChanged = ({statusChanged}: Meter) => statusChanged || <Separator/>;
+  const renderMedium = ({medium}: Meter) => medium;
 
   return (
     <Table result={result} entities={entities}>
