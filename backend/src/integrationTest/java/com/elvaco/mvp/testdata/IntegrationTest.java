f051fd2617bd179fc9362e1683586519eb595213
Anton Lofgren <antlof@elvaco.se>
2017-12-11 11:07:14 +0100

    Set DirtiesContext for IntegrationTest classes
    This seems like a reasonable default, to allow us to avoid worrying
    about working with a dirty state. At least when working against an
    embedded database.

--- backend/src/integrationTest/java/com/elvaco/mvp/testdata/IntegrationTest.java
+++ backend/src/integrationTest/java/com/elvaco/mvp/testdata/IntegrationTest.java
@@ -4,10 +4,14 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+// By default, remove application context after each class to avoid
+// contamination of unrelated tests
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 public abstract class IntegrationTest {
   @LocalServerPort
   private int serverPort;
