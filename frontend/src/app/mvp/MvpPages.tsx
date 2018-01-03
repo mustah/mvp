e00d898402f55a047067c342667c48861d555d4a
alexander laas <laas.alexander@gmail.com>
2018-01-03 13:48:17 +0100

    Remove auth-check for internal Mvp-pages

--- frontend/src/app/mvp/MvpPages.tsx
+++ frontend/src/app/mvp/MvpPages.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import {Redirect, Route, Switch} from 'react-router';
-import {userIsAuthenticated} from '../../services/authService';
 import {Collection} from '../../usecases/collection/components/Collection';
 import {DashboardContainer} from '../../usecases/dashboard/containers/DashboardContainer';
 import {ReportContainer} from '../../usecases/report/containers/ReportContainer';
@@ -8,20 +7,14 @@ import {SelectionContainer} from '../../usecases/selection/containers/SelectionC
 import {Validation} from '../../usecases/validation/components/Validation';
 import {routes} from '../routes';
 
-const DashboardPage = userIsAuthenticated(DashboardContainer);
-const CollectionPage = userIsAuthenticated(Collection);
-const ValidationPage = userIsAuthenticated(Validation);
-const ReportPage = userIsAuthenticated(ReportContainer);
-const SelectionPage = userIsAuthenticated(SelectionContainer);
-
 export const MvpPages = () => (
   <Switch>
-    <Route exact={true} path={routes.home} component={DashboardPage}/>
-    <Route exact={true} path={routes.dashboard} component={DashboardPage}/>
-    <Route exact={true} path={routes.collection} component={CollectionPage}/>
-    <Route exact={true} path={routes.validation} component={ValidationPage}/>
-    <Route exact={true} path={`${routes.report}/:id?`} component={ReportPage}/>
-    <Route exact={true} path={routes.selection} component={SelectionPage}/>
+    <Route exact={true} path={routes.home} component={DashboardContainer}/>
+    <Route exact={true} path={routes.dashboard} component={DashboardContainer}/>
+    <Route exact={true} path={routes.collection} component={Collection}/>
+    <Route exact={true} path={routes.validation} component={Validation}/>
+    <Route exact={true} path={`${routes.report}/:id?`} component={ReportContainer}/>
+    <Route exact={true} path={routes.selection} component={SelectionContainer}/>
     <Redirect to={routes.home}/>
   </Switch>
 );
