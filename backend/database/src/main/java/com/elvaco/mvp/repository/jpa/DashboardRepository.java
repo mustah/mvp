25a797d5bef771d741e2547b61a2c0597cd93016
Must Tahir <must.tahir@gmail.com>
2018-01-05 14:37:09 +0100

    Remove Dashboard* from backend
    Create this in a clean architecture way when it time. Json-server is
    handling the dashboards right now.

--- backend/database/src/main/java/com/elvaco/mvp/repository/jpa/DashboardRepository.java
+++
@@ -1,7 +0,0 @@
-package com.elvaco.mvp.repository.jpa;
-
-import com.elvaco.mvp.entity.dashboard.DashboardEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DashboardRepository extends JpaRepository<DashboardEntity, Long> {
-}
