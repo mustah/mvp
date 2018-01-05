c9d8584e9570b5d34197c392b04ec513362da2bc
Must Tahir <must.tahir@gmail.com>
2018-01-05 14:32:56 +0100

    Remove Report*

--- backend/database/src/main/java/com/elvaco/mvp/repository/jpa/ReportRepository.java
+++
@@ -1,7 +0,0 @@
-package com.elvaco.mvp.repository.jpa;
-
-import com.elvaco.mvp.entity.report.ReportEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReportRepository extends JpaRepository<ReportEntity, Long> {
-}
