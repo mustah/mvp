fc720487cd7dce079ad253789c2b95aa8a0bcfe1
alexander laas <laas.alexander@gmail.com>
2018-01-05 16:16:49 +0100

    Add form fields for user data in EditProfileContainer

--- frontend/src/usecases/topmenu/components/profile/EditProfileContainer.tsx
+++ frontend/src/usecases/topmenu/components/profile/EditProfileContainer.tsx
@@ -1,10 +1,25 @@
 import * as React from 'react';
+import {connect} from 'react-redux';
+import {Column} from '../../../../components/layouts/column/Column';
 import {Row} from '../../../../components/layouts/row/Row';
+import {Bold} from '../../../../components/texts/Texts';
 import {MainTitle} from '../../../../components/texts/Titles';
 import {MvpPageContainer} from '../../../../containers/MvpPageContainer';
+import {RootState} from '../../../../reducers/rootReducer';
 import {translate} from '../../../../services/translationService';
+import {User} from '../../../../state/domain-models/user/userModels';
+import 'EditProfileContainer.scss';
 
-export const EditProfileContainer = () => {
+interface StateToProps {
+  user: User;
+}
+
+type Props = StateToProps;
+
+const EditProfile = ({user}: Props) => {
+
+  const onSubmit = () => console.log('Submitting');
+
   return (
     <MvpPageContainer>
       <Row className="space-between">
@@ -12,6 +27,30 @@ export const EditProfileContainer = () => {
           {translate('profile')}
         </MainTitle>
       </Row>
+      <Column className="EditProfileContainer">
+        <form onSubmit={onSubmit}>
+          <Column>
+            <Bold className="first-uppercase">{translate('name')}:</Bold>
+            <input type="text" defaultValue={user.name} name="name"/>
+
+            <Bold className="first-uppercase">{translate('company')}:</Bold>
+            <input type="text" defaultValue={user.company.name} name="name"/>
+
+            <Bold className="first-uppercase">{translate('email')}:</Bold>
+            <input type="text" defaultValue={user.email} name="name"/>
+
+            <Bold className="first-uppercase">{translate('roles')}:</Bold>
+            <input type="text" defaultValue={user.roles} name="name" disabled={true}/>
+            <input type="submit" className="ProfileSave clickable" value={translate('save')}/>
+          </Column>
+        </form>
+      </Column>
     </MvpPageContainer>
   );
 };
+
+const mapStateToProps = ({auth: {user}}: RootState): StateToProps => (
+  {user: user!} // TODO: Perhaps use a selector instead of using the "!" null protection.
+);
+
+export const EditProfileContainer = connect<StateToProps>(mapStateToProps)(EditProfile);
