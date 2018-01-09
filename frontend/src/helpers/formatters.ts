2ff23cba7b0fc90fbd780fb2af4003f9b5a9a4ae
Carl Helmertz <carl.helmertz@elvaco.se>
2018-01-09 15:42:40 +0100

    helper: Create firstUpperTranslated()
    Some components don't allow us to pass down an entire component but just
    a text string. That means that we cannot style it with CSS as easily.
    
    A specifically repeated pattern is the "both translate, and then
    capitalize only the first word's first letter".
    
    Signed-off-by: Carl Helmertz <carl.helmertz@elvaco.se>

--- frontend/src/helpers/formatters.ts
+++ frontend/src/helpers/formatters.ts
@@ -1,5 +1,6 @@
 import * as moment from 'moment';
 import * as numeral from 'numeral';
+import {translate} from '../services/translationService';
 
 export const FORMAT_NUMBER_DEFAULT = '0,0.0[000]';
 export const FORMAT_DATE_DAY_MONTH = 'DD/MM';
@@ -7,3 +8,12 @@ export const FORMAT_DATE_DAY_MONTH = 'DD/MM';
 export const formatDate = (date: Date, format?: string) => moment(date).format(format || FORMAT_DATE_DAY_MONTH);
 export const formatNumber = (num: number | string, format?: string) =>
   numeral(num).format(format || FORMAT_NUMBER_DEFAULT);
+
+const firstUpper = (original: string): string => {
+  if (original.length === 0) {
+    return original;
+  }
+  return original[0].toUpperCase() + original.substr(1);
+};
+
+export const firstUpperTranslated = (original: string): string => firstUpper(translate(original));
