b9dd52805c3d4ad8948771e680a4d7e34558d4ab
alexander laas <laas.alexander@gmail.com>
2018-01-23 11:05:05 +0100

    Make profile name clickable

--- frontend/src/usecases/topmenu/components/profile/Profile.tsx
+++ frontend/src/usecases/topmenu/components/profile/Profile.tsx
@@ -4,13 +4,13 @@ import {Link} from 'react-router-dom';
 import {routes} from '../../../../app/routes';
 import {menuItemInnerDivStyle} from '../../../../app/themes';
 import {IconAvatar} from '../../../../components/icons/IconAvatar';
-import {Column} from '../../../../components/layouts/column/Column';
-import {Row} from '../../../../components/layouts/row/Row';
+import {Column, ColumnCenter} from '../../../../components/layouts/column/Column';
+import {Row, RowCenter} from '../../../../components/layouts/row/Row';
 import {PopoverMenu} from '../../../../components/popover/PopoverMenu';
 import {Xsmall} from '../../../../components/texts/Texts';
 import {translate} from '../../../../services/translationService';
 import {User} from '../../../../state/domain-models/user/userModels';
-import {uuid} from '../../../../types/Types';
+import {Clickable, uuid} from '../../../../types/Types';
 import './Profile.scss';
 
 interface Props {
@@ -21,10 +21,11 @@ interface Props {
 export const Profile = (props: Props) => {
   const {user, logout} = props;
   const logoutClick = () => logout(user.organisation.code);
+  const Icon = iconComponent({name: user.name});
   return (
     <Column className="ProfileWrapper">
       <Row className="Profile">
-        <PopoverMenu IconComponent={IconAvatar}>
+        <PopoverMenu IconComponent={Icon}>
           <Link to={routes.userProfile} className="link">
             <MenuItem
               style={menuItemInnerDivStyle}
@@ -42,9 +43,17 @@ export const Profile = (props: Props) => {
           </MenuItem>
         </PopoverMenu>
       </Row>
-      <Row className="Row-center">
-        <Xsmall className="Bold">{user.name}</Xsmall>
-      </Row>
     </Column>
   );
 };
+
+const iconComponent = ({name}: {name: string}) => (
+  (props: Clickable) => (
+    <ColumnCenter {...props} className="clickable">
+      <RowCenter>
+        <IconAvatar {...props} />
+      </RowCenter>
+      <Xsmall className="Bold">{name}</Xsmall>
+    </ColumnCenter>
+  )
+);
