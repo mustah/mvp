dd39ebece69f4beb827b303d23c92797d2009a88
Carl Helmertz <carl.helmertz@elvaco.se>
2018-01-24 17:00:49 +0100

    fuse: Write translation related files outside of frontend/src
    We should not write files to frontend/src/ during the build stage because the
    build becomes impossible to cache.
    
    Instead, we introduce the directory frontend/build/ where we
    temporarily place template.pot. We also write the JSON files containing
    the mapped translations, to the frontend/dist/ directory right away.
    
    This saves ~2 minutes for consequential builds triggered by
    ./gradlew :frontend:build
    
    Signed-off-by: Carl Helmertz <carl.helmertz@elvaco.se>

--- frontend/fuse-extract-translations.js
+++ frontend/fuse-extract-translations.js
@@ -3,6 +3,7 @@ const {defaultLanguage, defaultNamespace, i18nextConfig} = require('./src/i18n/i
 const fs = require('fs');
 const Parser = require('i18next-scanner').Parser;
 const converter = require('i18next-conv');
+const mkdirp = require('mkdirp');
 const {Sparky} = require('fuse-box');
 
 const utf8 = 'utf-8';
@@ -20,7 +21,7 @@ const parser = new Parser(parserOptions);
 const readFileContent = file => file.read().contents.toString(utf8);
 const accumulateContent = (prev, curr) => prev + curr;
 
-const createPotFile = async ({base}) => {
+const createPotFile = async ({base, outputDir}) => {
   return Sparky.src('**/*.+(ts|tsx)', {base})
     .completed(async (files) => {
       const content = files
@@ -33,7 +34,8 @@ const createPotFile = async ({base}) => {
       const json = parser.get()[defaultLanguage][defaultNamespace];
       const data = await converter.i18nextToPot(defaultLanguage, JSON.stringify(json), {quiet: true});
 
-      fs.writeFileSync('./src/i18n/locales/template.pot', data, utf8);
+      mkdirp.sync(outputDir);
+      fs.writeFileSync(`${outputDir}/template.pot`, data, utf8);
     })
     .exec();
 };
