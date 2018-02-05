14979a3a8ce17d2c9b340ca99c57e8f281ef3beb
Daniel Svensson <dansve@elvaco.se>
2018-02-05 15:04:58 +0100

    Remove endpoint for paged measurements

--- backend/core/src/main/java/com/elvaco/mvp/core/usecase/Measurements.java
+++ backend/core/src/main/java/com/elvaco/mvp/core/usecase/Measurements.java
@@ -5,23 +5,13 @@ import java.util.Map;
 import java.util.Optional;
 
 import com.elvaco.mvp.core.domainmodels.Measurement;
-import com.elvaco.mvp.core.spi.data.Page;
-import com.elvaco.mvp.core.spi.data.Pageable;
 
 public interface Measurements {
-
-  Page<Measurement> findAllByScale(
-    String scale,
-    Map<String, List<String>> filterParams,
-    Pageable pageable
-  );
 
   List<Measurement> findAllByScale(
     String scale,
     Map<String, List<String>> filterParams
   );
-
-  Page<Measurement> findAll(Map<String, List<String>> filterParams, Pageable pageable);
 
   List<Measurement> findAll(Map<String, List<String>> filterParams);
 
--- backend/web/src/main/java/com/elvaco/mvp/api/MeasurementController.java
+++ backend/web/src/main/java/com/elvaco/mvp/api/MeasurementController.java
@@ -5,15 +5,11 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import com.elvaco.mvp.core.domainmodels.Measurement;
-import com.elvaco.mvp.core.spi.data.Page;
 import com.elvaco.mvp.core.usecase.MeasurementUseCases;
 import com.elvaco.mvp.dto.MeasurementDto;
 import com.elvaco.mvp.exception.MeasurementNotFound;
-import com.elvaco.mvp.spring.PageableAdapter;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.Pageable;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -39,21 +35,6 @@ public class MeasurementController {
     return measurementUseCases.findById(id)
       .map(this::toDto)
       .orElseThrow(() -> new MeasurementNotFound(id));
-  }
-
-  @GetMapping
-  public org.springframework.data.domain.Page<MeasurementDto> measurements(
-    @RequestParam(value = "scale", required = false) String scale,
-    @RequestParam MultiValueMap<String, String> requestParams,
-    Pageable pageable
-  ) {
-    Page<Measurement> page = measurementUseCases.findAllPageable(
-      scale,
-      new HashMap<>(requestParams),
-      new PageableAdapter(pageable)
-    );
-    return new PageImpl<>(page.getContent(), pageable, page.getTotalElements())
-      .map(this::toDto);
   }
 
   @GetMapping
