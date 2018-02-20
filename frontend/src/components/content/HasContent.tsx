c4c3fc446e72a18f85edd74a99312c495a6a3158
alexander laas <laas.alexander@gmail.com>
2018-02-20 11:34:56 +0100

    Changes HasContent fallbackContent-typing
    - fallbackContent will only accept `React.ReactElement<any>`

--- frontend/src/components/content/HasContent.tsx
+++ frontend/src/components/content/HasContent.tsx
@@ -1,19 +1,17 @@
 import * as React from 'react';
-import './HasContent.scss';
-import classNames = require('classnames');
 
 interface Props {
   hasContent: boolean;
-  noContentText: string;
+  fallbackContent: React.ReactElement<any>;
   className?: string;
   children: React.ReactElement<any>;
 }
 
 export const HasContent = (props: Props) => {
-  const {className, hasContent, noContentText, children} = props;
+  const {hasContent, fallbackContent, children} = props;
 
   if (!hasContent) {
-    return (<h2 className={classNames('HasContent first-uppercase', className)}>{noContentText}</h2>);
+    return (fallbackContent);
   } else {
     return children;
   }
