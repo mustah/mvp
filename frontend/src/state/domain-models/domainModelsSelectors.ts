fd1099db09ddec99835ebee82ec56c0e0ece7e40
alexander laas <laas.alexander@gmail.com>
2018-02-23 11:51:44 +0100

    Fixes bug with entities undefined
    - When no measurements (other other entities) are returned from
    backend entities are set to undefined. This cause report view to fail.

--- frontend/src/state/domain-models/domainModelsSelectors.ts
+++ frontend/src/state/domain-models/domainModelsSelectors.ts
@@ -3,7 +3,8 @@ import {ErrorResponse, HasId, uuid} from '../../types/Types';
 import {DomainModel, NormalizedState, ObjectsById} from './domainModels';
 
 export const getResultDomainModels = <T extends HasId>(state: NormalizedState<T>): uuid[] => state.result;
-export const getEntitiesDomainModels = <T extends HasId>(state: NormalizedState<T>): ObjectsById<T> => state.entities;
+export const getEntitiesDomainModels = <T extends HasId>(state: NormalizedState<T>): ObjectsById<T> =>
+  state.entities || {};
 export const getDomainModel = <T extends HasId>({entities, result}: NormalizedState<T>): DomainModel<T> =>
   ({result, entities});
 
