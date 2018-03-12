78baccaa82c56bf64d705ac1df867dee92446d37
Carl Helmertz <carl.helmertz@elvaco.se>
2018-03-12 13:40:54 +0100

    rabbitmq: Split message parsing to one test file/parser

--- backend/rabbitmq-consumer/src/test/java/com/elvaco/mvp/consumers/rabbitmq/message/MeteringStructureParserTest.java
+++ backend/rabbitmq-consumer/src/test/java/com/elvaco/mvp/consumers/rabbitmq/message/MeteringStructureParserTest.java
@@ -10,7 +10,7 @@ import org.junit.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 
-public class MeteringMessageParserTest {
+public class MeteringStructureParserTest {
 
   @Test
   public void meteringStructureMessageIsParsedCorrectly() {
@@ -60,54 +60,6 @@ public class MeteringMessageParserTest {
   }
 
   @Test
-  public void meteringMeasurementMessageIsParsedCorrectly() {
-    String jsonMessage = "{\n"
-      + "  \"message_type\": \"Elvaco MVP MQ Measurement Message 1.0\",\n"
-      + "  \"gateway\": {\n"
-      + "    \"id\": \"GW-CME3100-XXYYZZ\"\n"
-      + "  },\n"
-      + "  \"meter\": {\n"
-      + "    \"id\": \"123456789\"\n"
-      + "  },\n"
-      + "  \"facility\": {\n"
-      + "    \"id\": \"42402519\"\n"
-      + "  },\n"
-      + "  \"organisation_id\": \"Elvaco AB\",\n"
-      + "  \"source_system_id\": \"Elvaco Metering\",\n"
-      + "  \"values\": [\n"
-      + "    {\n"
-      + "      \"timestamp\": 1506069947,\n"
-      + "      \"value\": 0.659,\n"
-      + "      \"unit\": \"wH\",\n"
-      + "      \"quantity\": \"power\",\n"
-      + "      \"status\": \"OK\"\n"
-      + "    }\n"
-      + "  ]\n"
-      + "}";
-    MeteringMessageParser messageParser = new MeteringMessageParser();
-    MeteringMeasurementMessageDto parsedMessage =
-      messageParser.parseMeasurementMessage(jsonMessage).orElse(null);
-
-    assertThat(parsedMessage).isNotNull();
-    assertThat(parsedMessage.messageType).isEqualTo(MessageType.METERING_MEASUREMENT_V_1_0);
-    assertThat(parsedMessage.gateway).isNotNull();
-    assertThat(parsedMessage.gateway.id).isEqualTo("GW-CME3100-XXYYZZ");
-    assertThat(parsedMessage.meter).isNotNull();
-    assertThat(parsedMessage.meter.id).isEqualTo("123456789");
-    assertThat(parsedMessage.facility).isNotNull();
-    assertThat(parsedMessage.facility.id).isEqualTo("42402519");
-    assertThat(parsedMessage.organisationId).isEqualTo("Elvaco AB");
-    assertThat(parsedMessage.sourceSystemId).isEqualTo("Elvaco Metering");
-    assertThat(parsedMessage.values).isNotNull();
-    assertThat(parsedMessage.values).hasSize(1);
-    ValueDto value = parsedMessage.values.get(0);
-    assertThat(value.timestamp).isEqualTo(1506069947);
-    assertThat(value.value).isEqualTo(0.659);
-    assertThat(value.unit).isEqualTo("wH");
-    assertThat(value.quantity).isEqualTo("power");
-  }
-
-  @Test
   public void parseMalformedStructureMessage() {
     MeteringMessageParser messageParser = new MeteringMessageParser();
     assertThat(messageParser.parseStructureMessage("")).isEmpty();
@@ -131,34 +83,4 @@ public class MeteringMessageParserTest {
     assertThat(messageParser.parseStructureMessage(jsonMessageMissingGateway)).isEmpty();
   }
 
-  @Test
-  public void parseMalformedMeasurementMessage() {
-    MeteringMessageParser messageParser = new MeteringMessageParser();
-    assertThat(messageParser.parseMeasurementMessage("")).isEmpty();
-    assertThat(messageParser.parseMeasurementMessage("{\"foo\": 1999}")).isEmpty();
-    assertThat(messageParser.parseMeasurementMessage("}}}}}}}}}}}}[]]}}}}}}}}}}¡")).isEmpty();
-
-    String jsonMessageWithoutValues = "{\n"
-      + "  \"message_type\": \"Elvaco MVP MQ Measurement Message 1.0\",\n"
-      + "  \"gateway\": {\n"
-      + "    \"id\": \"GW-CME3100-XXYYZZ\",\n"
-      + "    \"status\": \"OK\"\n"
-      + "  },\n"
-      + "  \"meter\": {\n"
-      + "    \"id\": \"123456789\",\n"
-      + "    \"status\": \"ERROR\"\n"
-      + "  },\n"
-      + "  \"facility_id\": \"42402519\",\n"
-      + "  \"organisation_id\": \"Elvaco AB\",\n"
-      + "  \"source_system_id\": \"Elvaco Metering\",\n"
-      + "  \"alarms\": [\n"
-      + "    {\n"
-      + "      \"timestamp\": 1506069947,\n"
-      + "      \"code\": 42,\n"
-      + "      \"description\": \"Low battery\"\n"
-      + "    }\n"
-      + "  ]\n"
-      + "}";
-    assertThat(messageParser.parseMeasurementMessage(jsonMessageWithoutValues)).isEmpty();
-  }
 }
