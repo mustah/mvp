2a3729560c53fb4280002ea8f2cc4904b0857376
Anton Lofgren <antlof@elvaco.se>
2018-03-13 10:35:33 +0100

    Mark MeasurementValueProjection methods @Nullable

--- backend/database/src/main/java/com/elvaco/mvp/database/repository/jpa/MeasurementValueProjection.java
+++ backend/database/src/main/java/com/elvaco/mvp/database/repository/jpa/MeasurementValueProjection.java
@@ -3,13 +3,16 @@ package com.elvaco.mvp.database.repository.jpa;
 import java.sql.Timestamp;
 import java.time.Instant;
 import java.util.Optional;
+import javax.annotation.Nullable;
 
 import com.elvaco.mvp.database.entity.measurement.MeasurementUnit;
 
 public interface MeasurementValueProjection {
 
+  @Nullable
   Timestamp getWhen();
 
+  @Nullable
   String getValue();
 
   default Optional<MeasurementUnit> getMeasurementUnit() {
@@ -19,14 +22,21 @@ public interface MeasurementValueProjection {
     return Optional.of(new MeasurementUnit(this.getValue()));
   }
 
+  @Nullable
   default Instant getInstant() {
+    Timestamp timestamp = getWhen();
+    if (timestamp == null) {
+      return null;
+    }
     return getWhen().toInstant();
   }
 
+  @Nullable
   default Double getValueValue() {
     return getMeasurementUnit().map(MeasurementUnit::getValue).orElse(null);
   }
 
+  @Nullable
   default String getUnit() {
     return getMeasurementUnit().map(MeasurementUnit::getUnit).orElse(null);
   }
