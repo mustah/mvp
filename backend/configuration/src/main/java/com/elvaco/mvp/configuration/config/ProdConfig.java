973a0b1a179b697cfb978502e558e17a4bdca26b
Must Tahir <must.tahir@gmail.com>
2018-03-16 15:53:35 +0100

    Do not use bcrypt when running system tests

--- backend/configuration/src/main/java/com/elvaco/mvp/configuration/config/ProdConfig.java
+++ backend/configuration/src/main/java/com/elvaco/mvp/configuration/config/ProdConfig.java
@@ -3,15 +3,15 @@ package com.elvaco.mvp.configuration.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.crypto.password.StandardPasswordEncoder;
 
-@Profile("h2")
+@Profile("prod")
 @Configuration
-class H2Config {
+class ProdConfig {
 
   @Bean
   PasswordEncoder passwordEncoder() {
-    return new StandardPasswordEncoder();
+    return new BCryptPasswordEncoder(13);
   }
 }
