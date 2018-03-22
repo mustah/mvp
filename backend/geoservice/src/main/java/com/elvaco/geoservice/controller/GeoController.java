5b7ff46ee6873f31fa9fdcaa83d9cebc6090b7ab
Erik Mellegård <erimel@elvaco.se>
2018-03-22 20:20:23 +0100

    Removed System.out and duplicate @RequestMapping.

--- backend/geoservice/src/main/java/com/elvaco/geoservice/controller/GeoController.java
+++ backend/geoservice/src/main/java/com/elvaco/geoservice/controller/GeoController.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,17 +15,13 @@ public class GeoController {
 
   @GetMapping("/byAddress")
   public String requestByAddress(GeoRequest request) {
-    System.out.println(request.getCallbackUrl());
-    System.out.println(request.getAddress());
     requestQueue.enqueueRequest(request);
     return "OK";
   }
 
-  @RequestMapping(value = "/byAddress")
-  @PostMapping
+
+  @PostMapping("/byAddress")
   public String requestByAddresspost(@RequestBody GeoRequest request) {
-    System.out.println(request.getCallbackUrl());
-    System.out.println(request.getAddress());
     requestQueue.enqueueRequest(request);
     return "OK";
   }
