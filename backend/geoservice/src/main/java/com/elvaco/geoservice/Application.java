e771d05e3f3c8d9663f933a03b0217679e5ba335
Erik Mellegård <erimel@elvaco.se>
2018-03-26 15:08:52 +0200

    Added @EnableJpaAuditing to make created and updateed on TimeStampedPersistableEntity work.

--- backend/geoservice/src/main/java/com/elvaco/geoservice/Application.java
+++ backend/geoservice/src/main/java/com/elvaco/geoservice/Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EntityScan(
@@ -11,6 +12,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 )
 @SpringBootApplication
 @EnableScheduling
+@EnableJpaAuditing
 public class Application {
 
   public static void main(String[] args) {
