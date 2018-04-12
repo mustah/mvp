358b7afba8bcad04e75fd9e60bc47ce5b610713e
Must Tahir <must.tahir@gmail.com>
2018-04-12 13:57:12 +0200

    Translate 'unknown' in selection drop downs

--- frontend/src/components/dropdown-selector/SimpleDropdownSelector.tsx
+++ frontend/src/components/dropdown-selector/SimpleDropdownSelector.tsx
@@ -1,22 +1,24 @@
 import * as React from 'react';
+import {translate} from '../../services/translationService';
 import {SelectionListItem} from '../../state/search/selection/selectionModels';
 import {Normal} from '../texts/Texts';
 import {DropdownProps, DropdownSelector} from './DropdownSelector';
 import './DropdownSelector.scss';
 
+const unknown = 'unknown';
+
 export const SimpleDropdownSelector = (props: DropdownProps) => {
 
-  const renderLabel = (index: number, filteredList: SelectionListItem[]) => {
+  const renderLabelAtIndex = (index: number, filteredList: SelectionListItem[]) => {
     const {name} = filteredList[index];
-    return (
-      <Normal>{name}</Normal>
-    );
+    const translatedName = name === unknown ? translate(unknown) : name;
+    return <Normal>{translatedName}</Normal>;
   };
 
   return (
     <DropdownSelector
       {...props}
-      renderLabel={renderLabel}
+      renderLabel={renderLabelAtIndex}
       rowHeight={30}
       visibleItems={10}
     />
