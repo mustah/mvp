832a53852d6b0c3cd0d23fd10402c1eac8ac6d2c
Mattias Ryrlén <mattias.ryrlen@elvaco.se>
2018-04-12 15:44:13 +0200

    Mark StepDefinitions as pending
    Signed-off-by: Mattias Ryrlén <mattias.ryrlen@elvaco.se>

--- acceptance_tests/src/test/java/cuke/StepDefinitions.java
+++ acceptance_tests/src/test/java/cuke/StepDefinitions.java
@@ -1,5 +1,6 @@
 package cuke;
 
+import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
@@ -7,22 +8,22 @@ import cucumber.api.java.en.When;
 public class StepDefinitions {
 
   @Given("I am on the login page")
-  public void givenIAmOnTheLoginPage() {
-    // PENDING
+  public void givenIAmOnTheLoginPage() throws Throwable {
+    throw new PendingException();
   }
 
   @When("I login as user '(.*)' and password '(.*)'")
-  public void whenILoginAsUserWithPassword(String user, String password){
-    // PENDING
+  public void whenILoginAsUserWithPassword(String user, String password) throws Throwable {
+    throw new PendingException();
   }
 
   @Then("I should see '(.*)'")
-  public void thenIShouldSee(String context) {
-    // PENDING
+  public void thenIShouldSee(String context) throws Throwable {
+    throw new PendingException();
   }
 
   @Then("I should not see '(.*)'")
-  public void thenIShouldNotSee(String context) {
-    // PENDING
+  public void thenIShouldNotSee(String context) throws Throwable {
+    throw new PendingException();
   }
 }
