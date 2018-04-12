72b89ecaf0f898035408cfdd152efeb41b2943ca
Must Tahir <must.tahir@gmail.com>
2018-04-12 09:56:56 +0200

    Remove gateway status from meter dialog
    Resolves: MVP-319

--- frontend/src/containers/dialogs/MeterDetailsInfo.tsx
+++ frontend/src/containers/dialogs/MeterDetailsInfo.tsx
@@ -5,7 +5,6 @@ import {Status} from '../../components/status/Status';
 import {MainTitle, Subtitle} from '../../components/texts/Titles';
 import {translate} from '../../services/translationService';
 import {Meter} from '../../state/domain-models-paginated/meter/meterModels';
-import {titleOf} from './dialogHelper';
 import {Info} from './Info';
 
 interface Props {
@@ -16,9 +15,6 @@ export const MeterDetailsInfo = ({meter}: Props) => {
   const renderAlarm = () => meter.alarm
     ? <Info label={translate('alarm')} value={meter.alarm}/>
     : null;
-
-  const gateway = meter.gateway;
-  const meterFlags = meter.flags || [];
 
   return (
     <Row>
@@ -41,10 +37,6 @@ export const MeterDetailsInfo = ({meter}: Props) => {
               <Subtitle>{translate('collection')}</Subtitle>
             </Row>
           </Column>
-          <Info
-            label={translate('status')}
-            value={<Status name={gateway.status.name}/>}
-          />
           <Info label={translate('interval')} value="24h"/>
           <Info label={translate('resolution')} value="1h"/>
         </Row>
@@ -59,7 +51,6 @@ export const MeterDetailsInfo = ({meter}: Props) => {
             value={<Status name={meter.status.name}/>}
           />
           {renderAlarm()}
-          <Info label={translate('flagged for action')} value={titleOf(meterFlags)}/>
         </Row>
         <Row>
           <Column>
