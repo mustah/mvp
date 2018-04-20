1201acd46a484df09f2d0f9435c3df6b9e165ab0
Mattias Ryrlén <mattias.ryrlen@elvaco.se>
2018-04-20 11:25:29 +0200

    cuke: Create/Delete users/organisations and update tests
    Signed-off-by: Mattias Ryrlén <mattias.ryrlen@elvaco.se>

--- acceptance_tests/src/acceptanceTest/java/cuke/StepDefinitions.java
+++ acceptance_tests/src/acceptanceTest/java/cuke/StepDefinitions.java
@@ -4,9 +4,15 @@ import java.net.InetAddress;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.TimeUnit;
 
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import cucumber.api.DataTable;
 import cucumber.api.Scenario;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
@@ -21,35 +27,45 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import support.ApiRequestHelper;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class StepDefinitions {
 
   private WebDriver driver;
-  private String mvpServerFinal;
+  private String mvpServer;
   private String mvpApiServer;
+  private String mvpAdminUsername;
+  private String mvpAdminPassword;
+  private String useLocalBrowser;
+  private List<JsonNode> organisations;
+  private ApiRequestHelper api;
+
+  private void getEnvironmentOptions() throws UnknownHostException {
+    String server = Optional.ofNullable(System.getenv("MVP_SERVER"))
+      .orElse("http://" + getHostName());
+    String webPort = Optional.ofNullable(System.getenv("MVP_WEB_PORT"))
+      .orElse("4444");
+    String apiPort = Optional.ofNullable(System.getenv("MVP_API_PORT"))
+      .orElse("8080");
+    useLocalBrowser = Optional.ofNullable(System.getenv("LOCAL_BROWSER"))
+      .orElse(null);
+    mvpAdminUsername = Optional.ofNullable(System.getenv("MVP_ADMIN_USERNAME"))
+      .orElse("mvpadmin@elvaco.se");
+    mvpAdminPassword = Optional.ofNullable(System.getenv("MVP_ADMIN_PASSWORD"))
+      .orElse("changeme");
+
+    mvpServer = server + ":" + webPort;
+    mvpApiServer = server + ":" + apiPort;
+  }
 
   @Before
-  public void setUp() throws MalformedURLException, UnknownHostException {
-    String mvpServer = Optional.ofNullable(System.getenv("MVP_SERVER"))
-      .orElse(getHostName());
-    String mvpPort = Optional.ofNullable(System.getenv("MVP_WEB_PORT"))
-      .orElse("4444");
-    String mvpApiPort = Optional.ofNullable(System.getenv("MVP_API_PORT"))
-      .orElse("8080");
-
-    mvpServerFinal = mvpServer + ":" + mvpPort;
-    mvpApiServer = mvpServer + ":" + mvpApiPort;
-
-    System.out.println("MVP_WEB_SERVER: " + mvpServerFinal);
-    System.out.println("MVP_API_SERVER: " + mvpApiServer);
-
-    String localBrowser = Optional.ofNullable(System.getenv("LOCAL_BROWSER"))
-      .orElse(null);
+  public void beforeScenario() throws MalformedURLException, UnknownHostException {
+    getEnvironmentOptions();
 
     ChromeOptions options = new ChromeOptions();
-    if (localBrowser != null) {
+    if (useLocalBrowser != null) {
       driver = new ChromeDriver(options);
     } else {
       String seleniumChromeStandaloneUrl = Optional.ofNullable(System.getenv("CHROME_URL"))
@@ -59,10 +75,16 @@ public class StepDefinitions {
     }
 
     driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+    System.out.println("MVP_WEB_SERVER: " + mvpServer);
+    System.out.println("MVP_API_SERVER: " + mvpApiServer);
+    api = new ApiRequestHelper(mvpApiServer, mvpAdminUsername, mvpAdminPassword);
+    organisations = new ArrayList<>();
   }
 
   @After
-  public void tearDown(Scenario scenario) {
+  public void afterScenario(Scenario scenario) {
+    api.deleteOrganisations(organisations);
     if (scenario.isFailed()) {
       final byte[] screenshot = ((TakesScreenshot) driver)
         .getScreenshotAs(OutputType.BYTES);
@@ -72,9 +94,29 @@ public class StepDefinitions {
     driver.quit();
   }
 
+  @Given("the following companies exist")
+  public void givenTheFollowingCompaniesExist(DataTable table) throws UnirestException {
+    List<Map<String, String>> data = table.asMaps(String.class, String.class);
+    for (Map<String, String> row : data) {
+      JsonNode organisation = api.createOrganisation(row.get("company"), row.get("slug"));
+      organisations.add(organisation);
+    }
+  }
+
+  @Given("the following users exist")
+  public void givenTheFollowingUsersExist(DataTable table) throws UnirestException {
+    List<Map<String,String>> data = table.asMaps(String.class, String.class);
+    for (Map<String, String> row: data) {
+      api.createUser(row.get("username"),
+        row.get("email"),
+        row.get("password"),
+        api.findOrganisationByName(row.get("organisation"), organisations));
+    }
+  }
+
   @Given("I am on the login page")
   public void givenIAmOnTheLoginPage() {
-    driver.get(mvpServerFinal);
+    driver.get(mvpServer);
     assertThat(driver.getTitle()).isEqualTo("Elvaco");
   }
 
--- acceptance_tests/src/acceptanceTest/resources/features/acceptance_test.feature
+++ acceptance_tests/src/acceptanceTest/resources/features/acceptance_test.feature
@@ -3,14 +3,23 @@ Feature: Acceptance test
     I want to be able to login
     So that I see that the site works
 
+    Background:
+        Given the following companies exist
+        | company                 | slug      |
+        | Acceptance Test Company | ac-comp   |
+        | Test Corp               | test-corp |
+        And the following users exist
+        | username | email          | password      | company   |
+        | at-user  | at@example.com | dummyPassw0rd | Test Corp |
+
     Scenario Outline: Login to MVP
         Given I am on the login page
         When I login as user '<user>' and password '<password>'
         Then I should be logged in as '<displayname>'
 
         Examples:
-            | user                 | password | displayname          |
-            | vaxjo@vaxjoenergi.se | vaxjo    | vaxjo@vaxjoenergi.se |
+            | user           | password      | displayname |
+            | at@example.com | dummyPassw0rd | at-user     |
 
     Scenario Outline: Deny login to MVP
         Given I am on the login page
@@ -18,5 +27,5 @@ Feature: Acceptance test
         Then I should see error message '<message>'
 
         Examples:
-            | user                    | password | message                               |
-            | blackbox-test@elvaco.se | nope     | Bad credentials |
+            | user           | password | message         |
+            | at@example.com | nope     | Bad credentials |
