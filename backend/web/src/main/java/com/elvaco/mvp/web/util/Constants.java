c56af7edbcd45901056fade6d83efbefc4e92014
Must Tahir <must.tahir@gmail.com>
2018-04-25 17:12:52 +0200

    Add @UtilityClass annotation for helper classes

--- backend/web/src/main/java/com/elvaco/mvp/web/util/Constants.java
+++ backend/web/src/main/java/com/elvaco/mvp/web/util/Constants.java
@@ -1,11 +1,12 @@
 package com.elvaco.mvp.web.util;
 
-public final class Constants {
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class Constants {
 
   public static final String API_V1 = "/api/v1";
   public static final String AUTHORIZATION = "Authorization";
   public static final String BEARER = "Bearer ";
   public static final String BASIC = "Basic ";
-
-  private Constants() {}
 }
