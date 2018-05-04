bba2fa7d127b3b7d7d8b58aa06e544ce5b230196
Must Tahir <must.tahir@gmail.com>
2018-05-04 10:23:27 +0200

    Make gateway and meters dialog look more alike

--- frontend/src/containers/dialogs/GatewayDetailsInfo.tsx
+++ frontend/src/containers/dialogs/GatewayDetailsInfo.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import {imagePathFor} from '../../app/routes';
-import {Column, ColumnCenter} from '../../components/layouts/column/Column';
+import {Column} from '../../components/layouts/column/Column';
 import {Row} from '../../components/layouts/row/Row';
 import {Status} from '../../components/status/Status';
 import {MainTitle} from '../../components/texts/Titles';
@@ -18,35 +18,25 @@ export const GatewayDetailsInfo = ({gateway}: Props) => {
   const gatewayImage = imagePathFor('cme2110.jpg');
 
   return (
-    <div className="GatewayDetailsInfo">
-      <Row className="space-between">
-        <Column>
+    <Column className="GatewayDetailsInfo">
+      <Column className="Overview">
+        <Row>
           <MainTitle>{translate('gateway details')}</MainTitle>
-        </Column>
-        <ColumnCenter>
-          <Row className="Address">
-            <Info label={translate('city')} value={locationNameTranslation(city.name)}/>
-            <Info label={translate('address')} value={locationNameTranslation(address.name)}/>
-          </Row>
-        </ColumnCenter>
-      </Row>
+          <Info label={translate('gateway serial')} value={serial}/>
+          <Info label={translate('product model')} value={productModel}/>
+          <Info label={translate('city')} value={locationNameTranslation(city.name)}/>
+          <Info label={translate('address')} value={locationNameTranslation(address.name)}/>
+        </Row>
+      </Column>
       <Row>
         <Column>
-          <img src={gatewayImage} width={100}/>
+          <img src={gatewayImage} width={120}/>
         </Column>
-        <Column className="Overview">
-          <Row>
-            <Info label={translate('gateway serial')} value={serial}/>
-            <Info label={translate('product model')} value={productModel}/>
-          </Row>
-          <Row>
-            <Info
-              label={translate('collection')}
-              value={<Status name={status.name}/>}
-            />
-          </Row>
-        </Column>
+        <Info
+          label={translate('collection')}
+          value={<Status name={status.name}/>}
+        />
       </Row>
-    </div>
+    </Column>
   );
 };
