1e279ce083b2cdc3d6a624059e5ba9583577700d
Must Tahir <must.tahir@gmail.com>
2018-05-16 16:45:59 +0200

    Show loading animation when fetch user selections from backend

--- frontend/src/usecases/sidemenu/containers/savedSelections/SavedSelectionsContainer.tsx
+++ frontend/src/usecases/sidemenu/containers/savedSelections/SavedSelectionsContainer.tsx
@@ -4,7 +4,6 @@ import ListItem from 'material-ui/List/ListItem';
 import * as React from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
-import './SavedSelectionsContainer.scss';
 import {
   dividerStyle,
   listItemStyle,
@@ -15,7 +14,8 @@ import {
   sideBarStyles,
 } from '../../../../app/themes';
 import {ConfirmDialog} from '../../../../components/dialog/DeleteConfirmDialog';
-import {Row} from '../../../../components/layouts/row/Row';
+import {Row, RowCenter} from '../../../../components/layouts/row/Row';
+import {LoadingSmall} from '../../../../components/loading/Loading';
 import {RootState} from '../../../../reducers/rootReducer';
 import {firstUpperTranslated} from '../../../../services/translationService';
 import {NormalizedState} from '../../../../state/domain-models/domainModels';
@@ -29,10 +29,12 @@ import {getSelection} from '../../../../state/user-selection/userSelectionSelect
 import {Callback, CallbackWithId, OnClick, uuid} from '../../../../types/Types';
 import {NoSavedSelections} from '../../components/savedSelections/NoSavedSelections';
 import {SavedSelectionActionsDropdown} from '../../components/savedSelections/SavedSelectionActionsDropdown';
+import './SavedSelectionsContainer.scss';
 
 interface StateToProps {
   selection: UserSelection;
   savedSelections: NormalizedState<UserSelection>;
+  isFetching: boolean;
 }
 
 interface DispatchToProps {
@@ -46,26 +48,18 @@ interface State {
   selectionToDelete?: uuid;
 }
 
+const innerDivStyle: React.CSSProperties = {padding: 0};
+
 class SavedSelections extends React.Component<StateToProps & DispatchToProps, State> {
 
   state: State = {isDeleteDialogOpen: false};
-
-  openDialog = (id: uuid) => this.setState({isDeleteDialogOpen: true, selectionToDelete: id});
-
-  closeDialog = () => this.setState({isDeleteDialogOpen: false});
-
-  deleteSelectedUser = () => this.props.deleteUserSelection(this.state.selectionToDelete!);
 
   componentDidMount() {
     this.props.fetchUserSelections();
   }
 
   render() {
-    const {savedSelections, selectSavedSelection, selection} = this.props;
-
-    const innerDivStyle: React.CSSProperties = {
-      padding: 0,
-    };
+    const {isFetching, savedSelections, selectSavedSelection, selection} = this.props;
 
     const renderListItem = (id: uuid) => {
       const item: UserSelection = savedSelections.entities[id];
@@ -96,10 +90,13 @@ class SavedSelections extends React.Component<StateToProps & DispatchToProps, St
 
     const listItems = savedSelections.result.length
       ? savedSelections.result.map(renderListItem)
-      : [<NoSavedSelections key={1}/>];
+      : isFetching
+        ? [<RowCenter key={'loading-small-1'}><LoadingSmall/></RowCenter>]
+        : [<NoSavedSelections key={1}/>];
 
     return (
       <List style={listStyle}>
+
         <ListItem
           className="ListItem"
           primaryText={firstUpperTranslated('saved selections')}
@@ -118,14 +115,21 @@ class SavedSelections extends React.Component<StateToProps & DispatchToProps, St
       </List>
     );
   }
+
+  openDialog = (id: uuid) => this.setState({isDeleteDialogOpen: true, selectionToDelete: id});
+
+  closeDialog = () => this.setState({isDeleteDialogOpen: false});
+
+  deleteSelectedUser = () => this.props.deleteUserSelection(this.state.selectionToDelete!);
 }
 
-const mapStateToProps = ({userSelection, domainModels: {userSelections}}: RootState): StateToProps => {
-  return {
-    selection: getSelection(userSelection),
-    savedSelections: userSelections,
-  };
-};
+const mapStateToProps =
+  ({userSelection, domainModels: {userSelections}}: RootState): StateToProps =>
+    ({
+      selection: getSelection(userSelection),
+      savedSelections: userSelections,
+      isFetching: userSelections.isFetching,
+    });
 
 const mapDispatchToProps = (dispatch): DispatchToProps => bindActionCreators({
   selectSavedSelection,
