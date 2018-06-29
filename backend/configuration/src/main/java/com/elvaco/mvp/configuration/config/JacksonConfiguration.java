6060c5ffc6034d2171c35d495385173c0a454c41
Must Tahir <must.tahir@gmail.com>
2018-06-29 11:48:19 +0200

    Make config class non-public - Spring finds them anyway

--- backend/configuration/src/main/java/com/elvaco/mvp/configuration/config/JacksonConfiguration.java
+++ backend/configuration/src/main/java/com/elvaco/mvp/configuration/config/JacksonConfiguration.java
@@ -6,12 +6,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class JacksonConfiguration {
+class JacksonConfiguration {
 
   @Bean
-  public Jackson2ObjectMapperBuilderCustomizer includeNonNullSerializer() {
+  Jackson2ObjectMapperBuilderCustomizer includeNonNullSerializer() {
     return jacksonObjectMapperBuilder ->
       jacksonObjectMapperBuilder.serializationInclusion(JsonInclude.Include.NON_NULL);
   }
-
 }
