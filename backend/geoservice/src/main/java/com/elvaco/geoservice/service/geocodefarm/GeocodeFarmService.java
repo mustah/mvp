7be2fcff56620f1d833100fa57c55dad00dbb88d
Must Tahir <must.tahir@gmail.com>
2018-07-30 22:28:07 +0200

    Handle NPE when quota is reached at geofarm service

--- backend/geoservice/src/main/java/com/elvaco/geoservice/service/geocodefarm/GeocodeFarmService.java
+++ backend/geoservice/src/main/java/com/elvaco/geoservice/service/geocodefarm/GeocodeFarmService.java
@@ -3,12 +3,14 @@ package com.elvaco.geoservice.service.geocodefarm;
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
 
 import com.elvaco.geoservice.repository.entity.Address;
 import com.elvaco.geoservice.repository.entity.GeoLocation;
 import com.elvaco.geoservice.service.AddressToGeoService;
+import farm.geocoding.beans.Account;
 import farm.geocoding.beans.GeocodingFarmResult;
-import farm.geocoding.beans.Result;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -31,9 +33,9 @@ public class GeocodeFarmService implements AddressToGeoService {
   private static final Map<String, String> COUNTRY_TO_CODE_MAP = new HashMap<>();
 
   static {
-    //Add all contries to the map, ie sweden->se, sverige->se, tyskland->de, germany->de
-    //TODO: some countries like US and GB can be named in more ways, like U.S.A. USA, United
-    // Kingdom etc. We will prabably need to address this one way or another in future.
+    //Add all countries to the map, ie sweden -> se, sverige -> se, tyskland -> de, germany -> de
+    // TODO: some countries like US and GB can be named in more ways, like U.S.A. USA, United
+    // TODO: Kingdom etc. We will probably need to address this one way or another in future.
     String[] locales = Locale.getISOCountries();
     for (String countryCode : locales) {
       Locale locale = new Locale("", countryCode);
@@ -55,14 +57,13 @@ public class GeocodeFarmService implements AddressToGeoService {
   @Override
   public GeoLocation getGeoByAddress(Address address) {
     String countryCode = COUNTRY_TO_CODE_MAP.get(address.getCountry().toLowerCase());
-    GeocodingFarmResult result = new RestTemplate()
-      .getForObject(
-        url,
-        GeocodingFarmResult.class,
-        address.street + " " + address.city + " " + address.country,
-        address,
-        countryCode
-      );
+    GeocodingFarmResult result = new RestTemplate().getForObject(
+      url,
+      GeocodingFarmResult.class,
+      address.street + " " + address.city + " " + address.country,
+      address,
+      countryCode
+    );
 
     return convert(result);
   }
@@ -92,18 +93,27 @@ public class GeocodeFarmService implements AddressToGeoService {
   }
 
   private GeoLocation convert(GeocodingFarmResult source) {
-    GeoLocation target = null;
-    if (source.getGeocodingResults().getResults() != null
-      && source.getGeocodingResults().getStatus().getResultCount() >= 1) {
-      target = new GeoLocation();
-      Result result = source.getGeocodingResults().getResults().get(0);
-      target.setLatitude(result.getCoordinates().getLatitude());
-      target.setLongitude(result.getCoordinates().getLongitude());
-      target.setConfidence(Accuracy.from(result.getAccuracy()).value);
-      target.setSource(getId());
+    adjustQuota(source.getGeocodingResults().getAccount());
+
+    return Optional.of(source.getGeocodingResults())
+      .filter(geocodingResults -> Objects.nonNull(geocodingResults.getStatus().getResultCount()))
+      .filter(geocodingResults -> geocodingResults.getStatus().getResultCount() >= 1)
+      .map(geocodingResults -> geocodingResults.getResults().get(0))
+      .map(result -> {
+        GeoLocation target = new GeoLocation();
+        target.setLatitude(result.getCoordinates().getLatitude());
+        target.setLongitude(result.getCoordinates().getLongitude());
+        target.setConfidence(Accuracy.from(result.getAccuracy()).value);
+        target.setSource(getId());
+        return target;
+      })
+      .orElse(null);
+  }
+
+  private void adjustQuota(Account account) {
+    if (account != null) {
+      this.quota =
+        Integer.parseInt(account.getUsageLimit()) - Integer.parseInt(account.getUsedToday());
     }
-    this.quota = Integer.parseInt(source.getGeocodingResults().getAccount().getUsageLimit())
-      - Integer.parseInt(source.getGeocodingResults().getAccount().getUsedToday());
-    return target;
   }
 }
