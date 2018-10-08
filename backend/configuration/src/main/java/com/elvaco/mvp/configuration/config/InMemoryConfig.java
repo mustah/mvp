31584ac1fae250c73878c459529bd39ef9b6f486
Anton Lofgren <antlof@elvaco.se>
2018-10-08 13:20:25 +0200

    Mute deprecation warning for StandardPasswordEncoder
    We don't really care that this is deprecated, since it shouldn't be used
    in a production configuration anyways.

--- backend/configuration/src/main/java/com/elvaco/mvp/configuration/config/InMemoryConfig.java
+++ backend/configuration/src/main/java/com/elvaco/mvp/configuration/config/InMemoryConfig.java
@@ -3,13 +3,15 @@ package com.elvaco.mvp.configuration.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.crypto.password.StandardPasswordEncoder;
 
 @Configuration
+/* StandardPasswordEncoder is deprecated, since it's not secure. We're well aware of this, but we
+want to use it anyways in this @Configuration to speed up testing */
+@SuppressWarnings("deprecation")
 class InMemoryConfig {
 
   @Bean
   PasswordEncoder inMemoryPasswordEncoder() {
-    return new StandardPasswordEncoder();
+    return new org.springframework.security.crypto.password.StandardPasswordEncoder();
   }
 }
