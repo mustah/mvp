855c9ce60f067342e6228915921586b1c867c7ea
Carl Helmertz <carl.helmertz@elvaco.se>
2018-10-18 11:10:47 +0200

    organisationModels: Add placeholder organisation
    This will be used as a default option when creating a new organisation.

--- frontend/src/state/domain-models/organisation/organisationModels.ts
+++ frontend/src/state/domain-models/organisation/organisationModels.ts
@@ -1,6 +1,13 @@
+import {firstUpperTranslated} from '../../../services/translationService';
 import {IdNamed, uuid} from '../../../types/Types';
 
 export interface Organisation extends IdNamed {
   slug: uuid;
   parent?: Organisation;
 }
+
+export const noOrganisation = (): Organisation => ({
+  id: -1,
+  name: firstUpperTranslated('no parent organisation'),
+  slug: 'none',
+});
