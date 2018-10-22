ef76c4d5991b140c6132933247d9b97718fa717c
Anton Lofgren <antlof@elvaco.se>
2018-10-22 15:30:52 +0200

    Verify Organisation validity

--- backend/core/src/main/java/com/elvaco/mvp/core/domainmodels/Organisation.java
+++ backend/core/src/main/java/com/elvaco/mvp/core/domainmodels/Organisation.java
@@ -3,7 +3,6 @@ package com.elvaco.mvp.core.domainmodels;
 import java.util.UUID;
 import javax.annotation.Nullable;
 
-import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
 
@@ -11,17 +10,14 @@ import static com.elvaco.mvp.core.util.Slugify.slugify;
 
 @EqualsAndHashCode
 @ToString
-@AllArgsConstructor
 public class Organisation implements Identifiable<UUID> {
 
   public final UUID id;
   public final String name;
   public final String slug;
   public final String externalId;
-
   @Nullable
   public final Organisation parent;
-
   @Nullable
   public final UserSelection selection;
 
@@ -31,7 +27,28 @@ public class Organisation implements Identifiable<UUID> {
     String slug,
     String externalId
   ) {
-    this(id, name, slugify(slug), externalId, null, null);
+    this(id, name, slug, externalId, null, null);
+  }
+
+  public Organisation(
+    UUID id,
+    String name,
+    String slug,
+    String externalId,
+    @Nullable Organisation parent,
+    @Nullable UserSelection selection
+  ) {
+
+    this.id = id;
+    this.name = name;
+    this.slug = slugify(slug);
+    this.externalId = externalId;
+    if (parent == null ^ selection == null) {
+      throw new IllegalArgumentException(
+        "Organisation needs either both 'parent' and 'selection', or none");
+    }
+    this.parent = parent;
+    this.selection = selection;
   }
 
   @Override
