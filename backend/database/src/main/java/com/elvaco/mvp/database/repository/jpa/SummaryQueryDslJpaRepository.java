8e35c593f23032592ed9198416c2179186dc776e
Anton Lofgren <antlof@elvaco.se>
2018-11-03 16:29:33 +0100

    Remove QueryFilters dependency from Summary repository

--- backend/database/src/main/java/com/elvaco/mvp/database/repository/jpa/SummaryQueryDslJpaRepository.java
+++ backend/database/src/main/java/com/elvaco/mvp/database/repository/jpa/SummaryQueryDslJpaRepository.java
@@ -1,23 +1,19 @@
 package com.elvaco.mvp.database.repository.jpa;
 
 import java.util.UUID;
-import javax.annotation.Nullable;
 import javax.persistence.EntityManager;
 
 import com.elvaco.mvp.core.domainmodels.MeterSummary;
+import com.elvaco.mvp.core.filter.Filters;
+import com.elvaco.mvp.core.filter.RequestParametersMapper;
 import com.elvaco.mvp.core.spi.data.RequestParameters;
 import com.elvaco.mvp.database.entity.meter.LogicalMeterEntity;
-import com.elvaco.mvp.database.repository.queryfilters.LogicalMeterQueryFilters;
-import com.elvaco.mvp.database.repository.queryfilters.PhysicalMeterStatusLogQueryFilters;
 import com.querydsl.core.types.Predicate;
 import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.JPQLQuery;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import static com.elvaco.mvp.database.repository.queryfilters.FilterUtils.isLocationQuery;
-import static com.elvaco.mvp.database.util.JoinIfNeededUtil.joinLogicalMeterGateways;
-import static com.elvaco.mvp.database.util.JoinIfNeededUtil.joinMeterAlarmLogs;
 import static com.querydsl.core.types.ExpressionUtils.allOf;
 import static com.querydsl.core.types.ExpressionUtils.isNotNull;
 
@@ -38,69 +34,45 @@ class SummaryQueryDslJpaRepository
    */
   @Override
   public MeterSummary summary(RequestParameters parameters) {
-    Predicate predicate = new LogicalMeterQueryFilters().toExpression(parameters);
-
-    long meters = countMeters(parameters, predicate);
-    long cities = countCities(parameters, predicate);
-    long addresses = countAddresses(parameters, predicate);
+    Filters filters = RequestParametersMapper.toFilters(parameters);
+    long meters = countMeters(filters);
+    long cities = countCities(filters);
+    long addresses = countAddresses(filters);
 
     return new MeterSummary(meters, cities, addresses);
   }
 
-  private long countMeters(RequestParameters parameters, Predicate predicate) {
-    JPQLQuery<?> query = createCountQuery(predicate);
-
-    applyJoins(query, parameters);
-
+  private long countMeters(Filters filters) {
+    JPQLQuery<?> query = createCountQuery();
+    new LogicalMeterFilterQueryDslVisitor().visitAndApply(filters, query);
     return query.distinct().fetchCount();
   }
 
-  private long countCities(RequestParameters parameters, Predicate predicate) {
-    JPQLQuery<?> query = createQuery(predicate)
-      .select(Expressions.list(LOCATION.country, LOCATION.city))
-      .where(hasCity(parameters));
-
-    applyJoins(query, parameters);
-
-    return (long) query.distinct().fetch().size();
+  private long countCities(Filters filters) {
+    JPQLQuery<?> query = createQuery()
+      .select(Expressions.list(LOCATION.country, LOCATION.city));
+    new LogicalMeterFilterQueryDslVisitor().visitAndApply(filters, query);
+    return (long) query.where(hasCity()).distinct().fetch().size();
   }
 
-  private long countAddresses(RequestParameters parameters, Predicate predicate) {
-    JPQLQuery<?> query = createQuery(predicate)
-      .select(Expressions.list(LOCATION.country, LOCATION.city, LOCATION.streetAddress))
-      .where(hasAddress(parameters));
+  private long countAddresses(Filters filters) {
+    JPQLQuery<?> query = createQuery()
+      .select(Expressions.list(LOCATION.country, LOCATION.city, LOCATION.streetAddress));
 
-    applyJoins(query, parameters);
+    new LogicalMeterFilterQueryDslVisitor().visitAndApply(filters, query);
 
-    return (long) query.distinct().fetch().size();
+    return (long) query.where(hasAddress()).distinct().fetch().size();
   }
 
-  private static JPQLQuery<?> applyJoins(JPQLQuery<?> query, RequestParameters parameters) {
-    query.leftJoin(LOGICAL_METER.location, LOCATION)
-      .leftJoin(LOGICAL_METER.physicalMeters, PHYSICAL_METER)
-      .leftJoin(PHYSICAL_METER.statusLogs, METER_STATUS_LOG)
-      .on(new PhysicalMeterStatusLogQueryFilters().toPredicate(parameters));
-
-    joinLogicalMeterGateways(query, parameters);
-    joinMeterAlarmLogs(query, parameters);
-
-    return query;
+  private static Predicate hasCity() {
+    return allOf(isNotNull(LOCATION.country), isNotNull(LOCATION.city));
   }
 
-  @Nullable
-  private static Predicate hasCity(RequestParameters parameters) {
-    return isLocationQuery(parameters)
-      ? null
-      : allOf(isNotNull(LOCATION.country), isNotNull(LOCATION.city));
-  }
-
-  private static Predicate hasAddress(RequestParameters parameters) {
-    return isLocationQuery(parameters)
-      ? null
-      : allOf(
-        isNotNull(LOCATION.country),
-        isNotNull(LOCATION.city),
-        isNotNull(LOCATION.streetAddress)
-      );
+  private static Predicate hasAddress() {
+    return allOf(
+      isNotNull(LOCATION.country),
+      isNotNull(LOCATION.city),
+      isNotNull(LOCATION.streetAddress)
+    );
   }
 }
