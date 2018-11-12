9fda9101c18b1ea6ed9fa492317bf1d0530121a2
Åsa Godin <asa.godin@elvaco.se>
2018-11-12 12:40:05 +0100

    Make UnitConverterConfig package private

--- backend/configuration/src/main/java/com/elvaco/mvp/configuration/config/UnitConverterConfig.java
+++ backend/configuration/src/main/java/com/elvaco/mvp/configuration/config/UnitConverterConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class UnitConverterConfig {
+class UnitConverterConfig {
 
   @Bean
   UnitConverter unitConverter() {
