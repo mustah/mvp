98e2081e3e6872aceec89520ac8ae867e050f42f
Carl Helmertz <carl.helmertz@elvaco.se>
2018-11-15 15:02:24 +0100

    Sub-organisation: Fail requests if invalid parent selection
    If the parent organisation's JSON blob is invalid, an exception was
    swallowed and the selection was not applied. That meant information
    leakage, since the sub-organisation user got access to all of the parent
    organisation's meters.

--- backend/configuration/src/integrationTest/java/com/elvaco/mvp/web/LogicalMeterControllerSubOrganisationTest.java
+++ backend/configuration/src/integrationTest/java/com/elvaco/mvp/web/LogicalMeterControllerSubOrganisationTest.java
@@ -1,16 +1,21 @@
 package com.elvaco.mvp.web;
 
+import java.util.UUID;
+
 import com.elvaco.mvp.core.domainmodels.LogicalMeter;
 import com.elvaco.mvp.core.domainmodels.Medium;
 import com.elvaco.mvp.core.domainmodels.Organisation;
 import com.elvaco.mvp.core.domainmodels.PhysicalMeter;
+import com.elvaco.mvp.core.domainmodels.User;
 import com.elvaco.mvp.core.domainmodels.UserSelection;
 import com.elvaco.mvp.core.spi.repository.UserSelections;
 import com.elvaco.mvp.testdata.IntegrationTest;
 import com.elvaco.mvp.testdata.Url;
+import com.elvaco.mvp.web.dto.ErrorMessageDto;
 import com.elvaco.mvp.web.dto.PagedLogicalMeterDto;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 
 import static com.elvaco.mvp.core.util.Json.toJsonNode;
 import static com.elvaco.mvp.testing.fixture.LocationTestData.kungsbacka;
@@ -26,12 +31,50 @@ public class LogicalMeterControllerSubOrganisationTest extends IntegrationTest {
 
   @Test
   public void excludeMetersWithUnknownCities() {
+    var logicalMeterId = randomUUID();
+    var user = subOrganisationUser(CITIES_JSON_STRING, logicalMeterId);
+
+    var content = as(user).getPage(Url.builder()
+      .path("/meters")
+      .build(), PagedLogicalMeterDto.class)
+      .getContent();
+
+    assertThat(content).extracting("id").containsExactly(logicalMeterId);
+  }
+
+  @Test
+  public void invalidSelectionThrowsException() {
+    var user = subOrganisationUser(
+      "{\"cities\":[\"sweden,perstorp\"],\"addresses\":[],"
+        + "\"alarms\":[],\"manufacturers\":[],"
+        + "\"productModels\":[],\"dateRange\":{\"period\":\"latest\"}}",
+      randomUUID()
+    );
+
+    var response = as(user).get(
+      Url.builder()
+        .path("/meters")
+        .build(),
+      ErrorMessageDto.class
+    );
+
+    assertThat(response.getBody().message).isEqualTo(
+      "Invalid configuration of sub-organisation, please contact support"
+    );
+    assertThat(response.getStatusCode()).isEqualTo(HttpStatus.FORBIDDEN);
+  }
+
+  private User subOrganisationUser(String parentOrganisationsSelection, UUID logicalMeterId) {
     var userSelection = userSelections.save(UserSelection.builder()
       .id(randomUUID())
       .name("a-user-selection")
       .ownerUserId(context().superAdmin.id)
       .organisationId(context().organisationId())
-      .selectionParameters(toJsonNode(CITIES_JSON_STRING))
+      .selectionParameters(
+        toJsonNode(
+          parentOrganisationsSelection
+        )
+      )
       .build());
 
     var subOrganisation = organisations.save(Organisation.builder()
@@ -53,6 +96,7 @@ public class LogicalMeterControllerSubOrganisationTest extends IntegrationTest {
       .build());
 
     var logicalMeterWithCity = logicalMeters.save(LogicalMeter.builder()
+      .id(logicalMeterId)
       .externalId("ex2")
       .organisationId(context().organisationId())
       .location(kungsbacka().build())
@@ -62,14 +106,7 @@ public class LogicalMeterControllerSubOrganisationTest extends IntegrationTest {
       .logicalMeterId(logicalMeterWithCity.id)
       .build());
 
-    var user = subOrgUser().organisation(subOrganisation).build();
-
-    var content = as(user).getPage(Url.builder()
-      .path("/meters")
-      .build(), PagedLogicalMeterDto.class)
-      .getContent();
-
-    assertThat(content).extracting("id").containsExactly(logicalMeterWithCity.id);
+    return subOrgUser().organisation(subOrganisation).build();
   }
 
   private PhysicalMeter.PhysicalMeterBuilder physicalMeterBuilder() {
