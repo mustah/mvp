09118c464491edef67f6818e78c8a16be176bb68
Carl Helmertz <carl.helmertz@elvaco.se>
2018-11-21 11:01:54 +0100

    TemporalResolution::fromString()
    This will be used when parsing parameters.

--- backend/core/src/main/java/com/elvaco/mvp/core/domainmodels/TemporalResolution.java
+++ backend/core/src/main/java/com/elvaco/mvp/core/domainmodels/TemporalResolution.java
@@ -1,11 +1,20 @@
 package com.elvaco.mvp.core.domainmodels;
 
 import java.time.Duration;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+import static java.util.function.Function.identity;
+import static java.util.stream.Collectors.toMap;
 
 public enum TemporalResolution {
   hour,
   day,
   month;
+
+  private static final Map<String, TemporalResolution> STRING_TO_ENUM = Stream.of(values())
+    .collect(toMap(Object::toString, identity()));
 
   public static TemporalResolution defaultResolutionFor(Duration duration) {
     if (duration.toDays() < 2) {
@@ -15,6 +24,10 @@ public enum TemporalResolution {
     } else {
       return month;
     }
+  }
+
+  public static Optional<TemporalResolution> fromString(String resolution) {
+    return Optional.ofNullable(STRING_TO_ENUM.get(resolution));
   }
 }
 
