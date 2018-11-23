ea5a9c4127572e74748c793dde19ecd85baf7567
Carl Helmertz <carl.helmertz@elvaco.se>
2018-11-23 10:49:04 +0100

    Selection: Fold sections (threshold+selection) on heading
    The heading is a much larger clickable target, than the little arrow
    icon.

--- frontend/src/components/icons/IconRightArrow.tsx
+++ frontend/src/components/icons/IconRightArrow.tsx
@@ -2,7 +2,7 @@ import IconButton from 'material-ui/IconButton';
 import NavigationArrowDropRight from 'material-ui/svg-icons/navigation-arrow-drop-right';
 import * as React from 'react';
 import {colors} from '../../app/themes';
-import {ClassNamed, Clickable} from '../../types/Types';
+import {ClassNamed} from '../../types/Types';
 import './Icons.scss';
 
 const iconSize: React.CSSProperties = {
@@ -16,14 +16,11 @@ const style: React.CSSProperties = {
   ...iconSize,
 };
 
-type Props = Clickable & ClassNamed;
-
-export const IconRightArrow = ({onClick, className}: Props) => (
+export const IconRightArrow = ({className}: ClassNamed) => (
   <IconButton
     className={className}
     style={style}
     iconStyle={iconSize}
-    onClick={onClick}
   >
     <NavigationArrowDropRight/>
   </IconButton>
