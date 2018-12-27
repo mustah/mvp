619fef127a96c9767ac891b304d9c51a2ade0c15
Must Tahir <must.tahir@gmail.com>
2018-12-27 16:32:26 +0100

    Upgrade i18next lib to latest version

--- frontend/src/services/translationService.ts
+++ frontend/src/services/translationService.ts
@@ -1,13 +1,13 @@
-import {Callback, InitOptions, TranslationOptions} from 'i18next';
+import {default as i18next} from 'i18next';
 import i18n from '../i18n/i18n';
 
-export const translate = (key: string, options?: TranslationOptions): string =>
+export const translate = (key: string, options?: i18next.TranslationOptions): string =>
   i18n.t(key, options);
 
-export const onTranslationInitialized = (callback: (options: InitOptions) => void) =>
+export const onTranslationInitialized = (callback: (options: i18next.InitOptions) => void) =>
   i18n.on('initialized', callback);
 
-export const changeTranslationLanguage = (language: string, callback?: Callback) =>
+export const changeTranslationLanguage = (language: string, callback?: i18next.Callback) =>
   i18n.changeLanguage(language, callback);
 
 export const firstUpper = (original: string): string =>
@@ -15,7 +15,7 @@ export const firstUpper = (original: string): string =>
     ? original
     : original[0].toUpperCase() + original.substr(1);
 
-export const firstUpperTranslated = (key: string, options?: TranslationOptions): string =>
+export const firstUpperTranslated = (key: string, options?: i18next.TranslationOptions): string =>
   firstUpper(translate(key, options));
 
 export const getI18nLanguage = (): string => i18n.language;
