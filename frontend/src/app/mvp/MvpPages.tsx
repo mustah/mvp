0b21ba2a3c2b2bf019e8deef80951886fd9b66ae
Must Tahir <must.tahir@gmail.com>
2019-01-24 12:37:03 +0100

    Fetch selection tree when loading report view

--- frontend/src/app/mvp/MvpPages.tsx
+++ frontend/src/app/mvp/MvpPages.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import {Redirect, Route, Switch} from 'react-router';
 import {DashboardContainer} from '../../usecases/dashboard/containers/DashboardContainer';
 import {MeterContainer} from '../../usecases/meter/containers/MeterContainer';
-import {ReportContainer} from '../../usecases/report/containers/ReportContainer';
+import {ReportPageContainer} from '../../usecases/report/containers/ReportPageContainer';
 import {SelectionContainer} from '../../usecases/selection/containers/SelectionContainer';
 import {EditProfileContainer} from '../../usecases/topmenu/containers/EditProfileContainer';
 import {routes} from '../routes';
@@ -12,7 +12,7 @@ export const MvpPages = () => (
     <Route exact={true} path={routes.home} component={DashboardContainer}/>
     <Route exact={true} path={routes.dashboard} component={DashboardContainer}/>
     <Route exact={true} path={routes.meter} component={MeterContainer}/>
-    <Route exact={true} path={`${routes.report}/:id?`} component={ReportContainer}/>
+    <Route exact={true} path={`${routes.report}/:id?`} component={ReportPageContainer}/>
     <Route exact={true} path={routes.selection} component={SelectionContainer}/>
     <Route exact={true} path={routes.userProfile} component={EditProfileContainer}/>
     <Redirect to={routes.home}/>
