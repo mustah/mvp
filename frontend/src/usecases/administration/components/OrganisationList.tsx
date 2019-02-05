037a7ad0036925323acd2e06ce72d8adaa5fa452
Carl Helmertz <carl.helmertz@elvaco.se>
2019-02-05 13:34:12 +0100

    admin/organisation: Render list as Kendo-<Grid>

--- frontend/src/usecases/administration/components/OrganisationList.tsx
+++ frontend/src/usecases/administration/components/OrganisationList.tsx
@@ -1,20 +1,15 @@
+import {Grid, GridColumn} from '@progress/kendo-react-grid';
+import {toArray} from 'lodash';
 import * as React from 'react';
 import {useConfirmDialog} from '../../../components/dialog/confirmDialogHook';
 import {ConfirmDialog} from '../../../components/dialog/DeleteConfirmDialog';
 import {Column} from '../../../components/layouts/column/Column';
 import {Row} from '../../../components/layouts/row/Row';
 import {Loader} from '../../../components/loading/Loader';
-import {Table, TableColumn} from '../../../components/table/Table';
-import {TableHead} from '../../../components/table/TableHead';
 import {translate} from '../../../services/translationService';
-import {Organisation} from '../../../state/domain-models/organisation/organisationModels';
 import {DispatchToProps, StateToProps} from '../containers/OrganisationsContainer';
 import {AddOrganisationButton} from './AddOrganisationButton';
 import {OrganisationActions} from './OrganisationActions';
-
-const renderParentOrganisation = ({parent}: Organisation) => parent ? parent.name : '-';
-const renderName = ({name}: Organisation) => name;
-const renderSlug = ({slug}: Organisation) => slug;
 
 type Props = StateToProps & DispatchToProps;
 
@@ -24,14 +19,15 @@ export const OrganisationList = ({
   error,
   fetchOrganisations,
   isFetching,
-  organisations,
+  organisations: {entities},
 }: Props) => {
   React.useEffect(() => {
     fetchOrganisations();
   });
   const {isOpen, openConfirm, closeConfirm, confirm} = useConfirmDialog(deleteOrganisation);
 
-  const renderActionDropdown = ({id}: Organisation) => <OrganisationActions confirmDelete={openConfirm} id={id}/>;
+  const parent = ({dataItem}) => <td>{dataItem.parent ? dataItem.parent.name : '-'}</td>;
+  const actions = ({dataItem: {id}}) => <td><OrganisationActions confirmDelete={openConfirm} id={id}/></td>;
 
   return (
     <Loader isFetching={isFetching} error={error} clearError={clearError}>
@@ -39,24 +35,16 @@ export const OrganisationList = ({
         <Row>
           <AddOrganisationButton/>
         </Row>
-        <Table {...organisations}>
-          <TableColumn
-            header={<TableHead className="first">{translate('name')}</TableHead>}
-            renderCell={renderName}
-          />
-          <TableColumn
-            header={<TableHead className="first">{translate('parent organisation')}</TableHead>}
-            renderCell={renderParentOrganisation}
-          />
-          <TableColumn
-            header={<TableHead>{translate('slug')}</TableHead>}
-            renderCell={renderSlug}
-          />
-          <TableColumn
-            header={<TableHead className="actionDropdown">{' '}</TableHead>}
-            renderCell={renderActionDropdown}
-          />
-        </Table>
+        <Grid
+          style={{borderTopWidth: 1}}
+          data={toArray(entities)}
+          scrollable="none"
+        >
+          <GridColumn field="name" title={translate('name')} headerClassName="left-most" className="left-most"/>
+          <GridColumn cell={parent} title={translate('parent organisation')}/>
+          <GridColumn field="slug" title={translate('slug')}/>
+          <GridColumn cell={actions} width={40}/>
+        </Grid>
         <ConfirmDialog isOpen={isOpen} close={closeConfirm} confirm={confirm}/>
       </Column>
     </Loader>
