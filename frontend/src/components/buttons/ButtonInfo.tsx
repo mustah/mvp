db076760bbffe483f7add4cb1fd8cd9292133522
Must Tahir <must.tahir@gmail.com>
2019-03-08 13:44:25 +0100

    Add link to meter from measurements legend list
    - Clean up some colors too
    - Added blue link color to meter details links
    
    Resolves: https://elvaco.atlassian.net/browse/MVP-1073

--- frontend/src/components/buttons/ButtonInfo.tsx
+++ frontend/src/components/buttons/ButtonInfo.tsx
@@ -1,3 +1,4 @@
+import classNames from 'classnames';
 import * as React from 'react';
 import {Titled} from '../../types/Types';
 import {Row, RowMiddle} from '../layouts/row/Row';
@@ -11,7 +12,7 @@ interface Props extends InfoButtonProps, Titled {
 }
 
 export const ButtonInfo = ({color, iconStyle, label, labelStyle, title}: Props) => (
-  <RowMiddle className="flex-nowrap">
+  <RowMiddle className={classNames('ButtonInfo', 'flex-nowrap')}>
     <InfoButton color={color} iconStyle={iconStyle}/>
     <Row className="ButtonInfo-Label" title={title}>
       <Normal style={labelStyle}>{label}</Normal>
