100cfada565cf4bed371148c4b650f731c119987
Must Tahir <must.tahir@gmail.com>
2019-03-08 11:08:49 +0100

    Rename file from MeterDetailsModels.ts to meterDetailsModels.ts
    Do not use uppercase for filenames that are not react components!

--- frontend/src/usecases/meter/measurements/meterDetailActions.ts
+++ frontend/src/usecases/meter/measurements/meterDetailActions.ts
@@ -2,4 +2,3 @@ import {createStandardAction} from 'typesafe-actions';
 import {SelectionInterval} from '../../../state/user-selection/userSelectionModels';
 
 export const setMeterDetailsTimePeriod = createStandardAction('SET_METER_DETAILS_TIME_PERIOD')<SelectionInterval>();
-
