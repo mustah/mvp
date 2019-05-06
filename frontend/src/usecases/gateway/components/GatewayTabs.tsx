f3554ba87d85dea211ff6e227764f8ba134e40e9
Daniel Svensson <dansve@elvaco.se>
2019-05-06 15:44:03 +0200

    Remove gateway components

--- frontend/src/usecases/gateway/components/GatewayTabs.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import {MainContentTabs, MainContentTabsProps} from '../../../components/tabs/components/MainContentTabs';
-import {GatewayListContainer} from '../containers/GatewayListContainer';
-
-export const GatewayTabs = (props: MainContentTabsProps) => (
-  <MainContentTabs {...props}>
-    <GatewayListContainer/>
-  </MainContentTabs>
-);
