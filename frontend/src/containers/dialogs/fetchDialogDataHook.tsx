b00847fdd5bfbf0546cd3a28f67c5854d417fdf5
Must Tahir <must.tahir@gmail.com>
2019-05-16 09:32:41 +0200

    Remove unused type

--- frontend/src/containers/dialogs/fetchDialogDataHook.tsx
+++ frontend/src/containers/dialogs/fetchDialogDataHook.tsx
@@ -1,16 +1,13 @@
 import * as React from 'react';
 import {User} from '../../state/domain-models/user/userModels';
 import {isSuperAdmin} from '../../state/domain-models/user/userSelectors';
-import {CallbackWithId, EncodedUriParameters, uuid} from '../../types/Types';
+import {CallbackWithId, uuid} from '../../types/Types';
 
 interface Props {
   fetchOrganisation: CallbackWithId;
   organisationId: uuid;
   user: User;
 }
-
-export type OnFetchGatewayMeterDetails =
-  (meterIds: uuid[], parameters: EncodedUriParameters, gatewayId?: uuid) => void;
 
 export const useFetchOrganisation = ({fetchOrganisation, organisationId, user}: Props): void => {
   React.useEffect(() => {
