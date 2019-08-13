5ee4f46cb5b84630fd3662126ed9efcce67b2327
Must Tahir <must.tahir@gmail.com>
2019-08-13 12:57:34 +0200

    Use pre and post method security
    Had to add authorities when creating the 'AbstractAuthenticationToken'
    which is used by spring's RoleVoter.
    
    With this commit it's possible to use annotation such as @PreAuthorize
    ("hasRole('SUPER_ADMIN') OR hasRole('MVP_ADMIN')");

--- backend/configuration/src/main/java/com/elvaco/mvp/configuration/config/WebSecurityConfig.java
+++ backend/configuration/src/main/java/com/elvaco/mvp/configuration/config/WebSecurityConfig.java
@@ -13,6 +13,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -28,6 +29,7 @@ import static com.elvaco.mvp.web.util.Constants.API_V1;
 
 @RequiredArgsConstructor
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 @Configuration
 class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
